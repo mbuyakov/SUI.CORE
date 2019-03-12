cbc8fb453bc4cad8c70addb0e18fc80e2f8e760b
el1191 <el1191@woyd.ru>
2019-03-12 16:20:27 +0300

    Fix unpublish script

--- unpublish.js
+++ unpublish.js
@@ -1,7 +1,7 @@
 const packagejson = require('./package.json');
 const spawn = require('child_process').spawn;
 
-spawn('npm', ['unpublish', `${packagejson.name}@${packagejson.version}`], {
+spawn('npm', ['unpublish', `${packagejson.name}@${packagejson.version}`, "--registry http://verdaccio.smp.sm-soft.ru/"], {
     cwd: process.cwd(),
     stdio: 'inherit',
     shell: true
