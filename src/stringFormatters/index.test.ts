408b3cacba05af54ee52d3b34f61c2d8bacc42b7
el1191 <el1191@woyd.ru>
2019-03-12 18:02:53 +0300

    Add stringFormatters

---
+++ src/stringFormatters/index.test.ts
@@ -0,0 +1,135 @@
+/* tslint:disable:no-import-side-effect no-implicit-dependencies typedef no-null-keyword */
+import * as chai from "chai";
+import "mocha";
+
+import * as stringFormatters from "./index";
+
+describe("StringFormatters", () => {
+  it("Uncapitalize null", () => {
+    chai.assert.equal(stringFormatters.unCapitalize(null), "");
+  });
+
+  it("Uncapitalize \" \"", () => {
+    chai.assert.equal(stringFormatters.unCapitalize(" "), " ");
+  });
+  it("Uncapitalize a", () => {
+    chai.assert.equal(stringFormatters.unCapitalize("a"), "a");
+  });
+  it("Uncapitalize A", () => {
+    chai.assert.equal(stringFormatters.unCapitalize("A"), "a");
+  });
+  it("Uncapitalize Aa", () => {
+    chai.assert.equal(stringFormatters.unCapitalize("Aa"), "aa");
+  });
+  it("Uncapitalize AA", () => {
+    chai.assert.equal(stringFormatters.unCapitalize("AA"), "AA");
+  });
+
+  it("Add plural ending null", () => {
+    chai.assert.equal(stringFormatters.addPluralEnding(null), "");
+  });
+
+  it("Add plural ending \" \"", () => {
+    chai.assert.equal(stringFormatters.addPluralEnding(" "), " ");
+  });
+
+  it("Add plural ending a", () => {
+    chai.assert.equal(stringFormatters.addPluralEnding("a"), "as");
+  });
+
+  it("Add plural ending y", () => {
+    chai.assert.equal(stringFormatters.addPluralEnding("y"), "ies");
+  });
+
+  it("Remove plural ending null", () => {
+    chai.assert.equal(stringFormatters.removePluralEnding(null), "");
+  });
+
+  it("Remove plural ending \" \"", () => {
+    chai.assert.equal(stringFormatters.removePluralEnding(" "), " ");
+  });
+
+  it("Remove plural ending as", () => {
+    chai.assert.equal(stringFormatters.removePluralEnding("as"), "a");
+  });
+
+  it("Remove plural ending ies", () => {
+    chai.assert.equal(stringFormatters.removePluralEnding("ies"), "y");
+  });
+
+  it("Format SQL timestamp null", () => {
+    chai.assert.equal(stringFormatters.formatSqlTimestamp(null), "");
+  });
+
+  it("Format SQL timestamp \" \"", () => {
+    chai.assert.equal(stringFormatters.formatSqlTimestamp(" "), " ");
+  });
+
+  it("Format SQL timestamp 2019-01-01T23:59:59.99999", () => {
+    chai.assert.equal(stringFormatters.formatSqlTimestamp("2019-01-01T23:59:59.99999"), "2019-01-01 23:59:59");
+  });
+
+  it("Format SQL timestamp aaaa", () => {
+    chai.assert.equal(stringFormatters.formatSqlTimestamp("aaaa"), "aaaa");
+  });
+
+  it("Format SQL timestamp to date null", () => {
+    chai.assert.equal(stringFormatters.formatSqlTimestampToDate(null), "");
+  });
+
+  it("Format SQL timestamp to date \" \"", () => {
+    chai.assert.equal(stringFormatters.formatSqlTimestampToDate(" "), " ");
+  });
+
+  it("Format SQL timestamp to date 2019-01-01T23:59:59.99999", () => {
+    chai.assert.equal(stringFormatters.formatSqlTimestampToDate("2019-01-01T23:59:59.99999"), "2019-01-01");
+  });
+
+  it("Format SQL timestamp to date aaaa", () => {
+    chai.assert.equal(stringFormatters.formatSqlTimestampToDate("aaaa"), "aaaa");
+  });
+
+  it("Format SQL timestamp to time null", () => {
+    chai.assert.equal(stringFormatters.formatSqlTimestampToTime(null), "");
+  });
+
+  it("Format SQL timestamp to time \" \"", () => {
+    chai.assert.equal(stringFormatters.formatSqlTimestampToTime(" "), " ");
+  });
+
+  it("Format SQL timestamp to time 2019-01-01T23:59:59.99999", () => {
+    chai.assert.equal(stringFormatters.formatSqlTimestampToTime("2019-01-01T23:59:59.99999"), "23:59:59");
+  });
+
+  it("Format SQL timestamp to time aaaa", () => {
+    chai.assert.equal(stringFormatters.formatSqlTimestampToTime("aaaa"), "aaaa");
+  });
+
+  it("Line feed screening null", () => {
+    chai.assert.equal(stringFormatters.lineFeedScreening(null), "");
+  });
+
+  it("Line feed screening \" \"", () => {
+    chai.assert.equal(stringFormatters.lineFeedScreening(" "), " ");
+  });
+
+  it("Line feed screening \"\n\n\"", () => {
+    chai.assert.equal(stringFormatters.lineFeedScreening("\n\n"), "\\n\\n");
+  });
+
+  it("Add quotes if string null", () => {
+    chai.assert.equal(stringFormatters.addQuotesIfString(null), null);
+  });
+
+  it("Add quotes if string \" \"", () => {
+    chai.assert.equal(stringFormatters.addQuotesIfString(" "), "\" \"");
+  });
+
+  it("Trim if string null", () => {
+    chai.assert.equal(stringFormatters.trimIfString(null), null);
+  });
+
+  it("Trim if string \" \"", () => {
+    chai.assert.equal(stringFormatters.trimIfString(" "), "");
+  });
+});
---
+++ src/stringFormatters/index.ts
@@ -0,0 +1,139 @@
+/**
+ * Capitalize first char in string
+ * @deprecated Use lodash
+ */
+export function capitalize(str: string): string {
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+/**
+ * Uncapitalize string.
+ * If input is null or undefined - return empty string
+ * If 1 and 2 symbol in upper case - return original string
+ */
+export function unCapitalize(str: string | null | undefined): string {
+  if (!str) {
+    return "";
+  }
+  // If 2 letter in upper case - return original string
+  if (str[1] && (str[1].toUpperCase() === str[1])) {
+    return str;
+  }
+
+  return str[0].toLowerCase() + str.slice(1);
+}
+
+/**
+ * Add plural ending to string
+ * If input is null - return empty string
+ */
+export function addPluralEnding(str: string | null | undefined): string {
+  if (!str) {
+    return "";
+  }
+  if (!str.trim()) {
+    return str;
+  }
+  let ret: string = str;
+
+  // If not with plural ending already
+  if (str[str.length - 1] !== "s") {
+    // If string end with "y" - replace ending to "ies"
+    if (str[str.length - 1] === "y") {
+      ret = `${str.slice(0, -1)}ie`;
+    }
+    ret += "s";
+  }
+
+  return ret;
+}
+
+/**
+ * Remove plural ending from string
+ * If input is null - return empty string
+ */
+export function removePluralEnding(str: string | null | undefined): string {
+  if (!str) {
+    return "";
+  }
+
+  return str.endsWith("ies") ? `${str.slice(0, -3)}y` : str.endsWith("s") ? str.slice(0, -1) : str;
+}
+
+const sqlTimestampRegexp: RegExp = /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})\.?\d*/;
+
+/**
+ * Format sql timestamp (2019-01-01T23:59:59.99999) to (2019-01-01 23:59:59)
+ * If input is null - return empty string
+ * If input cannot be formatted- return original string
+ */
+export function formatSqlTimestamp(timestamp: string | null | undefined): string {
+  if (!timestamp) {
+    return "";
+  }
+
+  return timestamp.replace(sqlTimestampRegexp, "$1-$2-$3 $4:$5:$6");
+}
+
+/**
+ * Format sql timestamp (2019-01-01T23:59:59.99999) to (2019-01-01)
+ * If input is null - return empty string
+ * If input cannot be formatted- return original string
+ */
+export function formatSqlTimestampToDate(timestamp: string | null | undefined): string | undefined {
+  if (!timestamp) {
+    return "";
+  }
+
+  return timestamp.replace(sqlTimestampRegexp, "$1-$2-$3");
+}
+
+/**
+ * Format sql timestamp (2019-01-01T23:59:59.99999) to (23:59:59)
+ * If input is null - return empty string
+ * If input cannot be formatted- return original string
+ */
+export function formatSqlTimestampToTime(timestamp: string | null | undefined): string | undefined {
+  if (!timestamp) {
+    return "";
+  }
+
+  return timestamp.replace(sqlTimestampRegexp, "$4:$5:$6");
+}
+
+/**
+ * Replace \n to \\n
+ * If input is null - return empty string
+ */
+export function lineFeedScreening(str: string | null | undefined): string {
+  if (!str) {
+    return "";
+  }
+
+  return str.replace(/\n/g, "\\n");
+}
+
+/**
+ * Replaced by lineFeedScreening
+ * @deprecated
+ * @see lineFeedScreening
+ */
+export function formatMultistringForGraphQL(str: string | null | undefined): string {
+  return lineFeedScreening(str);
+}
+
+/**
+ * If typeof input == string - return string in quotes
+ * In other case return original input
+ */
+export function addQuotesIfString<T>(payload: T): string | T {
+  return typeof payload === "string" ? `"${payload}"` : payload;
+}
+
+/**
+ * If typeof input == string - return trimmed string
+ * In other case return original input
+ */
+export function trimIfString<T>(payload: T): string | T {
+  return typeof payload === "string" ? payload.trim() : payload;
+}
