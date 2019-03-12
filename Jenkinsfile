e95edf440cd7ecd28ada981098561cbcf448fb4f
el1191 <el1191@woyd.ru>
2019-03-12 15:42:03 +0300

    Add tslint, add editorconfig

--- Jenkinsfile
+++ Jenkinsfile
@@ -17,6 +17,7 @@ pipeline {
             steps {
                 sh """
                 yarn install
+                yarn run lint
                 yarn build
                 """
             }
--- src/color/index.ts
+++ src/color/index.ts
@@ -1,18 +1,15 @@
+/**
+ * Color class with r,g,b,a channels
+ */
 export class Color {
-  public r: number;
-  public g: number;
-  public b: number;
-  public a: number;
 
-  constructor(r: number, g: number, b: number, a: number) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
-    this.a = a;
-  }
-
+  /**
+   * Convert HEX string (#FF00FF without alpha or #FF00FF00 with alpha) to Color class
+   */
   public static fromHex(hex: string): Color {
-    const match = hex.replace(/#/, '').match(/.{1,2}/g) as string[];
+    const match: string[] = hex.replace(/#/, "")
+      .match(/.{1,2}/g) as string[];
+
     return new Color(
       parseInt(match[0], 16),
       parseInt(match[1], 16),
@@ -21,16 +18,44 @@ export class Color {
     );
   }
 
+  /**
+   * Alpha channel
+   */
+  public a: number;
+  /**
+   * Blue channel
+   */
+  public b: number;
+  /**
+   * Green channel
+   */
+  public g: number;
+  /**
+   * Red channel
+   */
+  public r: number;
+
+  public constructor(r: number, g: number, b: number, a: number) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.a = a;
+  }
+
+  /**
+   * Convert Color to rgba format(rgba(255,0,255,0))
+   */
   public toRgba(): string {
     return `rgba(${this.r},${this.g},${this.b},${this.a})`;
   }
 }
 
-
-// left and right are colors that you're aiming to find
-// a color between. Percentage (0-100) indicates the ratio
-// of right to left. Higher percentage means more right,
-// lower means more left.
+/**
+ * Left and right are colors that you're aiming to find
+ * color between. Percentage (0-100) indicates the ratio
+ * of right to left. Higher percentage means more right,
+ * lower means more left.
+ */
 export function findColorBetween(left: Color, right: Color, percentage: number = 50): Color {
   return new Color(
     Math.round(left.r + (right.r - left.r) * percentage / 100),
