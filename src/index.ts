1e39e42545aa31f3b41bb385c5a3769c532766a0
el1191 <el1191@woyd.ru>
2019-03-13 17:14:36 +0300

    Rename style class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./color";
 export * from "./stringFormatters";
 export * from "./other";
+export * from "./styles";
---
+++ src/styles.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable:typedef */
+
+export const SUI_ROW_CONTAINER = "sui-row-container";
+export const SUI_ROW = "sui-row";
+export const SUI_ROW_GROW_LEFT = "sui-row-grow-left";
+export const SUI_ROW_GROW_RIGHT = "sui-row-grow-right";
