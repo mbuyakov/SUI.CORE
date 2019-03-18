7520c8eb2316d7c6c10793999b5fc0f07f2ce4a4
el1191 <el1191@woyd.ru>
2019-03-18 17:29:29 +0300

    Fix lint

---
+++ src/other.test.ts
@@ -0,0 +1,5 @@
+import * as other from "./other";
+
+test("Sleep 5ms async", async () => {
+  await other.sleep(5);
+});
--- src/other.ts
+++ src/other.ts
@@ -5,6 +5,6 @@ export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 /**
  * Return promise, that resolve after given ms
  */
-export function sleep(ms: number): Promise<void> {
+export async function sleep(ms: number): Promise<void> {
   return new Promise((resolve: () => void): Timeout => setTimeout(resolve, ms));
 }
