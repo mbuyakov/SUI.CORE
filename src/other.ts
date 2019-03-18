4a960cafdfc50ac7e77a295c4cf222723afa9c6b
el1191 <el1191@woyd.ru>
2019-03-18 17:09:46 +0300

    Add sleep function

--- src/other.ts
+++ src/other.ts
@@ -1,1 +1,10 @@
+import Timeout = NodeJS.Timeout;
+
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+/**
+ * Return promise, that resolve after given ms
+ */
+export function sleep(ms: number): Promise<void> {
+  return new Promise((resolve: () => void): Timeout => setTimeout(resolve, ms));
+}
