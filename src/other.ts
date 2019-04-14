c20799dd4cfa77437b1b27d8b7b275e661a884d5
el1191 <el1191@woyd.ru>
2019-04-14 16:10:36 +0300

    Add Rendered

--- src/other.ts
+++ src/other.ts
@@ -1,6 +1,9 @@
 import Timeout = NodeJS.Timeout;
+import * as React from "react";
 
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+export type Rendered<T extends React.Component> = React.ReactElement<T["props"]>;
 
 /**
  * Return promise, that resolve after given ms
