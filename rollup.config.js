6a165118c86355e17d49649a0662db829a80afd0
el1191 <el1191@woyd.ru>
2019-04-21 23:10:45 +0300

    Fix resolving dependencies in build

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import typescript from 'rollup-plugin-typescript2'
 import commonjs from 'rollup-plugin-commonjs'
 import json from 'rollup-plugin-json'
 import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
 import svgr from '@svgr/rollup'
 import * as ttypescript from 'ttypescript'
@@ -33,7 +32,6 @@ export default {
     }),
     url(),
     svgr(),
-    resolve(),
     tslint({
       throwOnError: true
     }),
--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,3 @@
-/* tslint:disable:typedef */
-
 export const SUI_ROW_CONTAINER = "sui-row-container";
 export const SUI_ROW = "sui-row";
 export const SUI_ROW_GROW_LEFT = "sui-row-grow-left";
