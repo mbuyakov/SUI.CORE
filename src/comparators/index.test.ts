746902e304f5c5939851b1a14d6d28c32d433c20
el1191 <el1191@woyd.ru>
2019-04-29 12:55:53 +0300

    Add nullFirst to date comparator

--- src/comparators/index.test.ts
+++ src/comparators/index.test.ts
@@ -20,16 +20,20 @@ test('[number] Compare "2.23" 3', () => {
 
 test("[date] Compare null null", () => {
   expect(comparators.formattedDateComparator(null, null)).toBe(0);
+  expect(comparators.formattedDateComparator(null, null, false)).toBe(0);
 });
 
 test('[date] Compare "" null', () => {
   expect(comparators.formattedDateComparator("", null)).toBe(0);
+  expect(comparators.formattedDateComparator("", null, false)).toBe(0);
 });
 
 test('[date] Compare "01.11.2019" null', () => {
-  expect(comparators.formattedDateComparator("01.11.2019", null)).toBe(1572566400000);
+  expect(comparators.formattedDateComparator("01.11.2019", null)).toBe(-1);
+  expect(comparators.formattedDateComparator("01.11.2019", null, false)).toBe(1572566400000);
 });
 
 test('[date] Compare "01.11.2019" "02.10.2019"', () => {
   expect(comparators.formattedDateComparator("01.11.2019", "02.10.2019")).toBe(2592000000);
+  expect(comparators.formattedDateComparator("01.11.2019", "02.10.2019", false)).toBe(2592000000);
 });
--- src/comparators/index.ts
+++ src/comparators/index.ts
@@ -12,11 +12,24 @@ const datePattern = /(\d{2})\.(\d{2})\.(\d{4})/;
  * Try to parse a and b to date and compare it
  */
 // tslint:disable-next-line:no-any
-export function formattedDateComparator(a: any, b: any): number {
+export function formattedDateComparator(a: any, b: any, nullFirst: boolean = true): number {
   const dateA = new Date(String(a).replace(datePattern,'$3-$2-$1'));
   const dateB = new Date(String(b).replace(datePattern,'$3-$2-$1'));
 
-  console.log(dateA.getMilliseconds());
+  const dateATime = dateA.getTime() || 0;
+  const dateBTime = dateB.getTime() || 0;
 
-  return ((dateA.getTime() || 0) - (dateB.getTime() || 0));
+  if(nullFirst && dateATime === 0 && dateBTime === 0) {
+    return 0;
+  }
+
+  if(nullFirst && dateATime === 0) {
+    return 1;
+  }
+
+  if(nullFirst && dateBTime === 0) {
+    return -1;
+  }
+
+  return (dateATime - dateBTime);
 }
