64d386152b78e8ee479682cb0e828a2821ed505b
HashAlexey <tiapkinalexey@gmail.com>
2019-05-07 14:31:48 +0300

    Improve comparators

--- src/comparators/index.ts
+++ src/comparators/index.ts
@@ -2,8 +2,20 @@
  * Try to cast a and b to number and compare it
  */
 // tslint:disable-next-line:no-any
-export function numberComparator(a: any, b: any): number {
-  return (Number(a) || 0) - (Number(b) || 0);
+export function numberComparator(a: any, b: any, nullFirst: boolean = true): number {
+  if(nullFirst && a === 0 && b === 0) {
+    return 0;
+  }
+
+  if(nullFirst && a === 0) {
+    return 1;
+  }
+
+  if(nullFirst && b === 0) {
+    return -1;
+  }
+
+  return (a - b);
 }
 
 const datePattern = /(\d{2})\.(\d{2})\.(\d{4})/;
@@ -19,17 +31,5 @@ export function formattedDateComparator(a: any, b: any, nullFirst: boolean = tru
   const dateATime = dateA.getTime() || 0;
   const dateBTime = dateB.getTime() || 0;
 
-  if(nullFirst && dateATime === 0 && dateBTime === 0) {
-    return 0;
-  }
-
-  if(nullFirst && dateATime === 0) {
-    return 1;
-  }
-
-  if(nullFirst && dateBTime === 0) {
-    return -1;
-  }
-
-  return (dateATime - dateBTime);
+  return numberComparator(dateATime, dateBTime, nullFirst);
 }
