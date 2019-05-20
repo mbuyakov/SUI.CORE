4dba36c23d0aedff1ac263c169b85f9eea7b5812
el1191 <el1191@woyd.ru>
2019-05-20 18:16:13 +0300

    Add asyncMap, Unpacked type

--- src/other/index.ts
+++ src/other/index.ts
@@ -1,3 +1,4 @@
+/* tslint:disable:no-any no-shadowed-variable */
 import * as React from "react";
 
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
@@ -14,6 +15,18 @@ export async function sleep(ms: number): Promise<void> {
 }
 
 export interface IObjectWithIndex {
-  // tslint:disable-next-line:no-any
   [index: string]: any;
 }
+
+/**
+ * Async map
+ */
+export async function asyncMap<T, U>(array: T[], fn: (item: T, index: number, originalArray: T[]) => Promise<U>): Promise<U[]> {
+  return Promise.all(array.map(fn));
+}
+
+export type Unpacked<T> =
+  T extends Array<infer U> ? U :
+    T extends (...args: any[]) => infer U ? U :
+      T extends Promise<infer U> ? U :
+        T;
