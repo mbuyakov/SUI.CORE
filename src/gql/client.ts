ed255b25d5bbb820ebd2441903f5f71af5c4f87a
el1191 <el1191@woyd.ru>
2019-05-23 21:11:06 +0300

    Share client between all instances of SUI core

--- src/gql/client.ts
+++ src/gql/client.ts
@@ -1,21 +1,26 @@
 import ApolloClient from "apollo-client";
 
-let CLIENT: ApolloClient<{}> | undefined;
+declare let window: Window & {
+  /**
+   * Variable for global client instance
+   */
+  SUI_CORE_CLIENT: ApolloClient<{}> | undefined;
+};
 
 /**
  * Must be call before use Gql wrappers
  */
 export function init(clientInstance: ApolloClient<{}>): void {
-  CLIENT = clientInstance;
+  window.SUI_CORE_CLIENT = clientInstance;
 }
 
 /**
  * Get client instance
  */
 export function getClient(): ApolloClient<{}> {
-  if (!CLIENT) {
+  if (!window.SUI_CORE_CLIENT) {
     throw new Error("Gql client not Initialized");
   }
 
-  return CLIENT;
+  return window.SUI_CORE_CLIENT;
 }
