501b59dc9d8f0ad151790d578a0925281a3bdbca
el1191 <el1191@woyd.ru>
2019-09-17 22:45:41 +0300

    feat: move BackendTable plugins

--- packages/SUI.ALL/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "@smsoft/sui-backend";
-export * from "@smsoft/sui-charts";
-export * from "@smsoft/sui-promised";
-export * from "@smsoft/sui-core";
-export * from "@smsoft/sui-base-components";
-export * from "@smsoft/sui-meta";
