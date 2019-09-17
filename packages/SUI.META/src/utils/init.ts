7c1f6943f1f09b268f5b82e3696605eb038593f5
el1191 <el1191@woyd.ru>
2019-09-17 19:45:06 +0300

    fix: lint

--- packages/SUI.META/src/utils/init.ts
+++ packages/SUI.META/src/utils/init.ts
@@ -2,6 +2,7 @@ import { IBaseCardTabLayout } from '@smsoft/sui-base-components/src/Base/BaseCar
 import { IUser } from '@smsoft/sui-core';
 
 import { ColumnInfo, ColumnInfoManager, NameManager, TableInfo, TableInfoManager } from '../cache';
+import { TableSettings } from '../TableSettings';
 
 import { parseRoutes, RouteType } from './metaUtils';
 
@@ -36,13 +37,13 @@ export interface IRawRoute {
 }
 
 export interface IMetaInitProps {
-  // Tmp
-  // tslint:disable-next-line:no-any
-  extraOmniTableSettingsTab?: IBaseCardTabLayout<any>;
   routes?: IRawRoute[];
   user?: IUser,
 
   defaultGetLinkForTable?(tableName: string, type: RouteType, id?: string | number): string | null;
+  // Tmp
+  // tslint:disable-next-line:no-any variable-name
+  extraOmniTableSettingsTab?(_this: TableSettings): IBaseCardTabLayout<any>;
   // tslint:disable-next-line:no-any
   routerPushFn(link: any): void;
 }
