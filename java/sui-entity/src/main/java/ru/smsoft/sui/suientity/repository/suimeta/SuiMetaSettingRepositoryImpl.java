c60994c138adfb29602b59ebb27131db4491f7c3
HashAlexey <tiapkinalexey@gmail.com>
2019-10-21 19:08:38 +0300

    Update meta settings

--- java/sui-entity/src/main/java/ru/smsoft/sui/suientity/repository/suimeta/SuiMetaSettingRepositoryImpl.java
+++ java/sui-entity/src/main/java/ru/smsoft/sui/suientity/repository/suimeta/SuiMetaSettingRepositoryImpl.java
@@ -8,7 +8,11 @@ import org.springframework.stereotype.Repository;
 import ru.smsoft.sui.suientity.entity.suimeta.TableInfo;
 import ru.smsoft.sui.suientity.enums.SettingKeys;
 
+import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Repository
 @Slf4j
@@ -21,9 +25,40 @@ public class SuiMetaSettingRepositoryImpl implements SuiMetaSettingRepository {
 
     @Override
     public String get(String settingKey) {
-        return jdbcTemplate.queryForObject(
-            String.format("SELECT value FROM sui_meta.sui_settings_kv WHERE key = '%s'", settingKey),
-            String.class);
+        return get(Collections.singleton(settingKey)).get(settingKey);
+    }
+
+    @Override
+    public Map<String, String> get(Iterable<String> settingKeys) {
+        val settingKeyList = StreamSupport
+                .stream(settingKeys.spliterator(), false)
+                .collect(Collectors.toList()) ;
+
+        if (!settingKeyList.isEmpty()) {
+            val queryResult = jdbcTemplate.queryForList(
+                    String.format(
+                            "SELECT key, value FROM sui_meta.sui_settings_kv WHERE key IN (%s)",
+                            settingKeyList
+                                .stream()
+                                .map(key -> String.format("'%s'", key))
+                                .collect(Collectors.joining(","))));
+
+            val result = queryResult
+                    .stream()
+                    .collect(Collectors.toMap(
+                        element -> (String) element.get("key"),
+                        element -> (String) element.get("value")));
+
+            settingKeyList.forEach(key -> {
+                if (!result.containsKey(key)) {
+                    result.put(key, null);
+                }
+            });
+
+            return result;
+        }
+
+        return Collections.emptyMap();
     }
 
     @Override
