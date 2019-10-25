d59d42fc70220f9eda8803362f6b13c01e193281
HashAlexey <tiapkinalexey@gmail.com>
2019-10-25 12:34:29 +0300

    Add system user

---
+++ java/sui-entity/src/main/resources/db/migration_sui/V6__add_system_user.sql
@@ -0,0 +1,3 @@
+-- Password rDjqzbZx5xPF98CfWzD3
+INSERT INTO sui_security.users(id, name, username, email, password)
+VALUES (-1, 'Системный пользователь', 'SYSTEM_USER', 'SYSTEM_USER', '$2a$10$9MsGo7aKXPnKEERsKp03NOCMO1vZjubpE/OzmvLo0vvlADOHgC1nC');
