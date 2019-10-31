38825ec54679cfea0afe5ae229d630cbf6888f8b
HashAlexey <tiapkinalexey@gmail.com>
2019-10-31 15:51:28 +0300

    MSS query fix (for MAT. VIEW)

--- java/sui-meta-schema-service/src/main/resources/queries/get-information-schema-columns.sql
+++ java/sui-meta-schema-service/src/main/resources/queries/get-information-schema-columns.sql
@@ -1,9 +1,15 @@
-SELECT table_schema,
-       table_name,
-       column_name,
-       column_default,
-       (udt_name::REGTYPE)::TEXT AS column_type,
-       (is_nullable = 'YES') AS is_nullable
-FROM information_schema.columns
-WHERE table_schema IN (%s)
-ORDER BY ordinal_position;
+SELECT pg_namespace.nspname AS table_schema,
+       pg_class.relname AS table_name,
+       pg_attribute.attname AS column_name,
+       pg_attrdef.adsrc AS column_default,
+       (pg_attribute.atttypid::REGTYPE)::TEXT,
+       NOT pg_attribute.attnotnull AS is_nullable
+FROM pg_catalog.pg_class
+         INNER JOIN pg_catalog.pg_namespace ON pg_class.relnamespace = pg_namespace.oid
+         INNER JOIN pg_catalog.pg_attribute ON pg_class.oid = pg_attribute.attrelid
+         LEFT JOIN pg_catalog.pg_attrdef ON (pg_attribute.attrelid = pg_attrdef.adrelid AND pg_attribute.attnum = pg_attrdef.adnum)
+WHERE pg_class.relkind IN ('r', 'm', 'v')
+  AND NOT pg_attribute.attisdropped
+  AND pg_attribute.attnum >= 1
+  AND pg_namespace.nspname IN (%s)
+ORDER BY pg_attribute.attnum;
--- java/sui-meta-schema-service/src/main/resources/queries/get-information-schema-tables.sql
+++ java/sui-meta-schema-service/src/main/resources/queries/get-information-schema-tables.sql
@@ -1,3 +1,11 @@
-SELECT table_schema, table_name, table_type
-FROM information_schema.tables
-WHERE table_schema IN (%s);
+SELECT pg_namespace.nspname AS table_schema,
+       pg_class.relname AS table_name,
+       CASE
+           WHEN relkind = 'r' THEN 'BASE TABLE'
+           WHEN relkind = 'v' THEN 'VIEW'
+           WHEN relkind = 'm' THEN 'MATERIALIZED VIEW'
+           END AS table_type
+FROM pg_catalog.pg_class
+     INNER JOIN pg_catalog.pg_namespace ON pg_class.relnamespace = pg_namespace.oid
+WHERE pg_class.relkind IN ('r', 'm', 'v')
+  AND pg_namespace.nspname IN (%s)
