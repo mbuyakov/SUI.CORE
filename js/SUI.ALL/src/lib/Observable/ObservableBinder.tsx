ac83e377a616477e9927f0793c621dc9dbaa7b74
el1191 <el1191@woyd.ru>
2019-11-07 12:27:05 +0300

    Fix

--- js/SUI.ALL/src/lib/Observable/ObservableBinder.tsx
+++ js/SUI.ALL/src/lib/Observable/ObservableBinder.tsx
@@ -1,12 +1,15 @@
-import {Observable, SUIReactComponent} from "@smsoft/sui-all";
 import React from "react";
+
+import { SUIReactComponent } from '../SUIReactComponent';
+
+import { Observable } from './Observable';
 
 export interface IObservableBinderProps<T> {
   observable: Observable<T>
   children(value: T): React.ReactNode
 }
 
-export default class ObservableBinder<T> extends SUIReactComponent<IObservableBinderProps<T>, {
+export class ObservableBinder<T> extends SUIReactComponent<IObservableBinderProps<T>, {
   value: T
 }> {
   public constructor(props: IObservableBinderProps<T>) {
