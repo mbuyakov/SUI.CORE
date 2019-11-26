5279c9db67ea87ef156188a8151f212feea0878b
Nikita Kostin <n.kostin11@mail.ru>
2019-11-26 13:31:07 +0300

    Fix delete filter

--- js/SUI.ALL/src/lib/BaseTable/filters/CustomSelectFilter.tsx
+++ js/SUI.ALL/src/lib/BaseTable/filters/CustomSelectFilter.tsx
@@ -39,7 +39,7 @@ export class CustomSelectFilter<T extends string | number>
     this.props.onFilter({
       columnName: this.props.column.name,
       operation: "equal",
-      raw: false,
+      raw: true,
       value: value as any
     } as unknown as Filter);
   }
