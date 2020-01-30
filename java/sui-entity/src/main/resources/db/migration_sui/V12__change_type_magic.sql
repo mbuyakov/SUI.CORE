3de5d878b21b90a13873249a46b54506432b1cff
HashAlexey <tiapkinalexey@gmail.com>
2020-01-30 12:41:59 +0300

    V12 fix

--- java/sui-entity/src/main/resources/db/migration_sui/V12__change_type_magic.sql
+++ java/sui-entity/src/main/resources/db/migration_sui/V12__change_type_magic.sql
@@ -20,38 +20,28 @@ WITH full_column_info AS (
 INSERT INTO ____tmp____comments(comment)
 SELECT format(
     'COMMENT ON COLUMN %s.%s.%s IS ''@references %s.%s(%s)'';',
-    schema_name,
-    table_name,
-    column_name,
-    foreign_schema_name,
-    foreign_table_name,
-    foreign_column_name
+    t1.table_schema,
+    t1.table_name,
+    t1.column_name,
+    t1.ref_info[1],
+    t1.ref_info[2],
+    t1.ref_info[3]
 )
 FROM (
-    SELECT fci1.schema_name,
-           fci1.table_name,
-           fci1.column_name,
-           fci2.schema_name AS foreign_schema_name,
-           fci2.table_name AS foreign_table_name,
-           fci2.column_name AS foreign_column_name
-    FROM sui_meta.column_info_reference
-    INNER JOIN full_column_info fci1 ON column_info_reference.column_info_id = fci1.column_info_id
-    INNER JOIN full_column_info fci2 ON column_info_reference.foreign_column_info_id = fci2.column_info_id
-    EXCEPT
-    SELECT tc.table_schema,
-           tc.table_name,
-           kcu.column_name,
-           ccu.table_schema,
-           ccu.table_name,
-           ccu.column_name
-    FROM information_schema.table_constraints AS tc
-        INNER JOIN information_schema.key_column_usage AS kcu
-            ON tc.constraint_name = kcu.constraint_name AND tc.table_schema = kcu.table_schema
-        INNER JOIN information_schema.constraint_column_usage AS ccu
-            ON ccu.constraint_name = tc.constraint_name
-                   AND tc.table_schema = ccu.constraint_schema
-    WHERE tc.constraint_type = 'FOREIGN KEY'
-) t;
+    SELECT pg_namespace.nspname AS table_schema,
+           pg_class.relname AS table_name,
+           pg_attribute.attname AS column_name,
+           regexp_matches(pg_description.description, '@references (.+?)\.(.+?)\((.+?)\)', 'i') AS ref_info
+    FROM pg_catalog.pg_class
+        INNER JOIN pg_catalog.pg_namespace ON pg_class.relnamespace = pg_namespace.oid
+        INNER JOIN pg_catalog.pg_attribute ON pg_class.oid = pg_attribute.attrelid
+        INNER JOIN pg_catalog.pg_description
+            ON (pg_attribute.attrelid = pg_description.objoid AND pg_attribute.attnum = pg_description.objsubid)
+    WHERE pg_class.relkind IN ('r', 'm', 'v')
+        AND NOT pg_attribute.attisdropped
+        AND pg_attribute.attnum >= 1
+        AND pg_description.description ILIKE '%%@references%%'
+) t1;
 
 CREATE FUNCTION ____WTF____()
     RETURNS VOID
