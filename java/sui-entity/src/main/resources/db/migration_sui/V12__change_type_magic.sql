fc5b0db6d7374912207338eb5df93fbdcbc2f215
HashAlexey <tiapkinalexey@gmail.com>
2020-01-30 11:25:03 +0300

    Fix V12

--- java/sui-entity/src/main/resources/db/migration_sui/V12__change_type_magic.sql
+++ java/sui-entity/src/main/resources/db/migration_sui/V12__change_type_magic.sql
@@ -4,6 +4,55 @@ CREATE TABLE ____tmp____ (
     drop_statement TEXT NOT NULL
 );
 
+CREATE TABLE ____tmp____comments (
+    id BIGSERIAL PRIMARY KEY,
+    comment TEXT NOT NULL
+);
+
+WITH full_column_info AS (
+    SELECT schema_name,
+           table_name,
+           column_name,
+           column_info.id AS column_info_id
+    FROM sui_meta.column_info
+    INNER JOIN sui_meta.table_info ON column_info.table_info_id = table_info.id
+)
+INSERT INTO ____tmp____comments(comment)
+SELECT format(
+    'COMMENT ON COLUMN %s.%s.%s IS ''@references %s.%s(%s)'';',
+    schema_name,
+    table_name,
+    column_name,
+    foreign_schema_name,
+    foreign_table_name,
+    foreign_column_name
+)
+FROM (
+    SELECT fci1.schema_name,
+           fci1.table_name,
+           fci1.column_name,
+           fci2.schema_name AS foreign_schema_name,
+           fci2.table_name AS foreign_table_name,
+           fci2.column_name AS foreign_column_name
+    FROM sui_meta.column_info_reference
+    INNER JOIN full_column_info fci1 ON column_info_reference.column_info_id = fci1.column_info_id
+    INNER JOIN full_column_info fci2 ON column_info_reference.foreign_column_info_id = fci2.column_info_id
+    EXCEPT
+    SELECT tc.table_schema,
+           tc.table_name,
+           kcu.column_name,
+           ccu.table_schema,
+           ccu.table_name,
+           ccu.column_name
+    FROM information_schema.table_constraints AS tc
+        INNER JOIN information_schema.key_column_usage AS kcu
+            ON tc.constraint_name = kcu.constraint_name AND tc.table_schema = kcu.table_schema
+        INNER JOIN information_schema.constraint_column_usage AS ccu
+            ON ccu.constraint_name = tc.constraint_name
+                   AND tc.table_schema = ccu.constraint_schema
+    WHERE tc.constraint_type = 'FOREIGN KEY'
+) t;
+
 CREATE FUNCTION ____WTF____()
     RETURNS VOID
     LANGUAGE plpgsql
@@ -16,6 +65,7 @@ $$
         drop_statement TEXT;
         ____tmp____id TEXT;
         processed BIGINT[] = ARRAY[]::BIGINT[];
+        comment TEXT;
     BEGIN
         SELECT COUNT(*)
         FROM pg_catalog.pg_views
@@ -56,10 +106,15 @@ $$
         ) LOOP
             EXECUTE create_statement;
         END LOOP;
+
+        FOR comment IN ( SELECT ____tmp____comments.comment FROM ____tmp____comments) LOOP
+            EXECUTE comment;
+        END LOOP;
     END;
 $$;
 
 SELECT ____WTF____();
 
 DROP TABLE ____tmp____;
+DROP TABLE ____tmp____comments;
 DROP FUNCTION ____WTF____();
