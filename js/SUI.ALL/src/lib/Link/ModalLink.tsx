66d3bd66f20a1bd8ea43bc0347eeb37c100dbcbd
HashAlexey <tiapkinalexey@gmail.com>
2020-02-07 18:24:24 +0300

    Fix Router import

--- js/SUI.ALL/src/lib/Link/ModalLink.tsx
+++ js/SUI.ALL/src/lib/Link/ModalLink.tsx
@@ -1,9 +1,8 @@
 // tslint:disable:max-classes-per-file class-name variable-name
-import {Router} from "@material-ui/icons";
 import Button, {ButtonType} from 'antd/lib/button/button';
 import autobind from 'autobind-decorator';
 import * as React from 'react';
-import { RouteComponentProps, withRouter } from 'react-router';
+import { RouteComponentProps, Router, withRouter } from 'react-router';
 
 import { draw } from '../utils';
 
