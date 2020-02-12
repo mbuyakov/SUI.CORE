1047dbe73628dc499c0f4868fd3c7d26e8b46373
Maksim Khlystov <maxvls@gmail.com>
2020-02-12 17:12:25 +0300

    Add db unique validator to index

--- js/SUI.ALL/src/lib/validator/index.ts
+++ js/SUI.ALL/src/lib/validator/index.ts
@@ -1,5 +1,6 @@
 export * from "./const";
 export * from "./dateValidation";
+export * from "./DBUniqueValidator";
 export * from "./innValidator";
 export * from "./nameValidator";
 export * from "./utils";
