f1d815cf3ef680cc76749ff41ed54fedd94e0d94
HashAlexey <tiapkinalexey@gmail.com>
2020-02-14 17:01:08 +0300

    Add watch filter to *SelectWithWaitData

--- js/SUI.ALL/src/lib/Inputs/TreeSelectWithWaitData.tsx
+++ js/SUI.ALL/src/lib/Inputs/TreeSelectWithWaitData.tsx
@@ -1,35 +1,84 @@
 import {TreeSelect} from 'antd';
 import {TreeNode, TreeSelectProps} from 'antd/lib/tree-select';
 import {TreeNodeValue} from "antd/lib/tree-select/interface";
+import autobind from "autobind-decorator";
 import classNames from "classnames";
 import * as React from "react";
 
 import {TableInfoManager} from "../cache";
 import {getDataByKey} from "../dataKey";
 import {IGqlFilter} from "../gql";
-import {ExtractProps} from "../other";
 import {TREE_SELECT_DISABLE_GROUP_SELECTION} from "../styles";
 import {getDataSet, getDataSetRender, getUser, IDataSet} from "../utils";
 
-export class TreeSelectWithWaitData<TValue extends TreeNodeValue, TValueType = {}, TGroupType = {}>
-  extends React.Component<Omit<TreeSelectProps<TValue>, 'treeData' | 'treeNodeFilterProp'> & {
+export type ITreeSelectWithWaitDataProps<TValue extends TreeNodeValue, TValueType, TGroupType> =
+  Omit<TreeSelectProps<TValue>, 'treeData' | 'treeNodeFilterProp'> & {
   disableGroupSelection?: boolean;
   groupTableFilter?: IGqlFilter<TGroupType>;
   groupTableIdentifier: string;
   valueTableFilter?: IGqlFilter<TValueType>;
   valueTableIdentifier: string;
-}, {
+  watchFilter?: boolean;
+}
+
+export interface ITreeSelectWithWaitDataState {
   dataSet?: IDataSet[];
   ready?: boolean;
   treeData?: TreeNode[];
-}> {
+}
 
-  public constructor(props: ExtractProps<TreeSelectWithWaitData<TValue, TValueType, TGroupType>>) {
+export class TreeSelectWithWaitData<TValue extends TreeNodeValue, TValueType = {}, TGroupType = {}>
+  extends React.Component<ITreeSelectWithWaitDataProps<TValue, TValueType, TGroupType>, ITreeSelectWithWaitDataState> {
+
+  public constructor(props: ITreeSelectWithWaitDataProps<TValue, TValueType, TGroupType>) {
     super(props);
     this.state = {};
   }
 
   public async componentDidMount(): Promise<void> {
+    return this.updateData();
+  }
+
+  public async componentDidUpdate(prevProps: Readonly<ITreeSelectWithWaitDataProps<TValue, TValueType, TGroupType>>): Promise<void> {
+    if (this.props.watchFilter) {
+      if (JSON.stringify(this.props.valueTableFilter) !== JSON.stringify(prevProps.valueTableFilter)
+        || JSON.stringify(this.props.groupTableFilter) !== JSON.stringify(prevProps.groupTableFilter)) {
+        await this.updateData();
+      }
+    }
+  }
+
+  public getData(): IDataSet[] {
+    return this.state.dataSet;
+  }
+
+  public render(): JSX.Element {
+    const ready = this.state.ready;
+
+    return (
+      <TreeSelect
+        allowClear={true}
+        showSearch={true}
+        showCheckedStrategy="SHOW_PARENT"
+        {...this.props}
+        style={{...this.props.style, ...(!ready && {filter: 'blur(0.5px)'})}}
+        treeData={this.state.treeData}
+        treeNodeFilterProp="title"
+        dropdownClassName={classNames(
+          this.props.dropdownClassName,
+          this.props.disableGroupSelection && TREE_SELECT_DISABLE_GROUP_SELECTION
+        )}
+        placeholder={this.props.placeholder || getDataByKey(this.state.treeData, 0, "children", 0, "title") || null}
+      />
+    );
+  }
+
+  @autobind
+  private async updateData(): Promise<void> {
+    if (this.state.ready) {
+      this.setState({ready: false});
+    }
+
     const groupTableInfo = this.props.groupTableIdentifier && await TableInfoManager.getById(this.props.groupTableIdentifier);
     const valueTableInfo = await TableInfoManager.getById(this.props.valueTableIdentifier);
 
@@ -63,31 +112,6 @@ export class TreeSelectWithWaitData<TValue extends TreeNodeValue, TValueType = {
           value: `${groupTableInfo.tableName}-${group.id}`
         }))
     });
-  }
-
-  public getData(): IDataSet[] {
-    return this.state.dataSet;
-  }
-
-  public render(): JSX.Element {
-    const ready = this.state.ready;
-
-    return (
-      <TreeSelect
-        allowClear={true}
-        showSearch={true}
-        showCheckedStrategy="SHOW_PARENT"
-        {...this.props}
-        style={{...this.props.style, ...(!ready && {filter: 'blur(0.5px)'})}}
-        treeData={this.state.treeData}
-        treeNodeFilterProp="title"
-        dropdownClassName={classNames(
-          this.props.dropdownClassName,
-          this.props.disableGroupSelection && TREE_SELECT_DISABLE_GROUP_SELECTION
-        )}
-        placeholder={this.props.placeholder || getDataByKey(this.state.treeData, 0, "children", 0, "title") || null}
-      />
-    );
   }
 
 }
