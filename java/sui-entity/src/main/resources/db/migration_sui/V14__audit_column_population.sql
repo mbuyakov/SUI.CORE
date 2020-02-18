49ae16f48d47ff6fe4eac1d5b98ca427c535a99a
HashAlexey <tiapkinalexey@gmail.com>
2020-02-18 18:32:26 +0300

    V14 fix #2

--- java/sui-entity/src/main/resources/db/migration_sui/V14__audit_column_population.sql
+++ java/sui-entity/src/main/resources/db/migration_sui/V14__audit_column_population.sql
@@ -231,6 +231,32 @@ $$
     END;
 $$;
 
+-- Без ограничения на аудит метасхемы
+CREATE OR REPLACE FUNCTION log.stop_audit_table(table_info_id bigint)
+    RETURNS VOID
+    LANGUAGE plpgsql
+AS
+$$
+    DECLARE
+        table_oid BIGINT;
+    BEGIN
+        SELECT sui_meta.get_table_info_oid(table_info_id) INTO table_oid;
+
+        IF table_oid IS NULL THEN
+            RAISE EXCEPTION 'Не удалось идентифицировать таблицу с table_info_id = %', table_info_id;
+        END IF;
+
+        EXECUTE 'DROP TRIGGER IF EXISTS audit_log_trigger ON ' || table_oid::regclass::text || ';';
+        EXECUTE 'DROP TRIGGER IF EXISTS populate_audit_columns_trigger ON ' || table_oid::regclass::text || ';';
+
+        UPDATE sui_meta.table_info SET is_audited = false WHERE id = table_info_id;
+    END;
+$$;
+
+SELECT log.stop_audit_table(id) FROM sui_meta.table_info WHERE is_audited IS TRUE;
+SELECT log.start_audit_table(id) FROM sui_meta.table_info WHERE is_audited IS TRUE;
+
+-- Возвращаем ограничение на аудит метасхемы
 CREATE OR REPLACE FUNCTION log.stop_audit_table(table_info_id bigint)
     RETURNS VOID
     LANGUAGE plpgsql
@@ -255,6 +281,3 @@ $$
         UPDATE sui_meta.table_info SET is_audited = false WHERE id = table_info_id;
     END;
 $$;
-
-SELECT log.stop_audit_table(id) FROM sui_meta.table_info WHERE is_audited IS TRUE;
-SELECT log.start_audit_table(id) FROM sui_meta.table_info WHERE is_audited IS TRUE;
