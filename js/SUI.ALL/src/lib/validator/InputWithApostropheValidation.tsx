50062d2e9a7f285e05effeccc1b8553769c45049
Nikita Kostin <n.kostin11@mail.ru>
2020-02-25 13:56:28 +0300

    Fix InputWithApostropheValidation to core

--- js/SUI.ALL/src/lib/validator/InputWithApostropheValidation.tsx
+++ js/SUI.ALL/src/lib/validator/InputWithApostropheValidation.tsx
@@ -1,25 +1,27 @@
+/* tslint:disable:triple-equals */
 import {Button, Input, Popover} from 'antd';
-import React, {ChangeEvent} from 'react';
-import autobind from 'autobind-decorator';
 import {InputProps} from "antd/lib/input/Input";
+import autobind from 'autobind-decorator';
+import React, {ChangeEvent} from 'react';
 
 
 export type ValidatorFunc = (name: string | null) => string;
 
 export function apostropheValidator(value: string | null): string {
   if (value && value.trim().length) {
-    let statePrefix = getAcceptStateFromPrefix(value);
+    const statePrefix = getAcceptStateFromPrefix(value);
     if (statePrefix !== null && statePrefix !== AcceptState.START) {
       return 'Ошибочный апостроф в фамилии';
     }
   }
+
   return '';
 }
 
+// tslint:disable-next-line:typedef
 export function wrapWithApostropheValidator(validator: ValidatorFunc) {
-  return (value: string | null) => {
-    return apostropheValidator(value) || validator(value);
-  };
+  return (value: string | null) =>
+    apostropheValidator(value) || validator(value);
 }
 
 export type InputWithApostropheValidationProps = InputProps & {
@@ -27,13 +29,13 @@ export type InputWithApostropheValidationProps = InputProps & {
   onChange?(value: string): void
 }
 
-type InputWithApostropheValidationState = {
+interface IInputWithApostropheValidationState {
   acceptState: AcceptState,
   focused: boolean,
 }
 
-export class InputWithApostropheValidation extends React.Component<InputWithApostropheValidationProps, InputWithApostropheValidationState> {
-  constructor(props: InputWithApostropheValidationProps) {
+export class InputWithApostropheValidation extends React.Component<InputWithApostropheValidationProps, IInputWithApostropheValidationState> {
+  public constructor(props: InputWithApostropheValidationProps) {
     super(props);
     this.state = {
       acceptState: AcceptState.START,
@@ -42,7 +44,7 @@ export class InputWithApostropheValidation extends React.Component<InputWithApos
   }
 
   public componentDidUpdate(prevProps: InputWithApostropheValidationProps,
-                            prevState: InputWithApostropheValidationState): void {
+                            prevState: IInputWithApostropheValidationState): void {
     if (prevState.acceptState != this.state.acceptState) {
       const hasApostrophe = countApostrophes(this.props.value as string) > 0;
       this.props.onChange(getPrefixedValue(this.props.value as string,
@@ -52,6 +54,7 @@ export class InputWithApostropheValidation extends React.Component<InputWithApos
 
   public render(): React.ReactNode {
     const clearValue: string = dropPrefix(this.props.value as string || "");
+
     return (
       <div>
         <Popover
@@ -74,42 +77,7 @@ export class InputWithApostropheValidation extends React.Component<InputWithApos
   }
 
   @autobind
-  private onChange(e: ChangeEvent<HTMLInputElement>): void {
-    const apCount = countApostrophes(e.target.value);
-    const newAcceptState = this.getNewAcceptStateOnChange(apCount);
-    if (newAcceptState !== this.state.acceptState) {
-      this.setState({acceptState: newAcceptState});
-    }
-    const newValue = getPrefixedValue(e.target.value, newAcceptState, apCount > 0);
-    if (this.props.value !== newValue) {
-      this.props.onChange(newValue);
-    }
-  }
-
-  @autobind
-  private onFocus(): void {
-    this.setState({focused: true});
-  }
-
-  @autobind
-  private onBlur(): void {
-    setTimeout(() => {
-      this.setState({focused: false});
-    }, 100);
-  }
-
-  @autobind
-  private getNewAcceptStateOnChange(apostrophesCount: number) {
-    if (apostrophesCount === 0) {
-      return AcceptState.START;
-    }
-    if (this.state.acceptState === AcceptState.START) {
-      return AcceptState.ASKING_USER;
-    }
-    return this.state.acceptState;
-  }
-
-  @autobind
+  // tslint:disable-next-line:typedef
   private getAcceptDeclineButtons() {
     return (
       <span>
@@ -124,13 +92,53 @@ export class InputWithApostropheValidation extends React.Component<InputWithApos
   }
 
   @autobind
+  // tslint:disable-next-line:typedef
+  private getNewAcceptStateOnChange(apostrophesCount: number) {
+    if (apostrophesCount === 0) {
+      return AcceptState.START;
+    }
+    if (this.state.acceptState === AcceptState.START) {
+      return AcceptState.ASKING_USER;
+    }
+
+    return this.state.acceptState;
+  }
+
+  @autobind
+  // tslint:disable-next-line:typedef
   private onAcceptBtnClickHandler() {
     this.setState({acceptState: AcceptState.ACCEPTED});
   }
 
   @autobind
+  private onBlur(): void {
+    setTimeout(() => {
+      this.setState({focused: false});
+    }, 100);
+  }
+
+  @autobind
+  private onChange(e: ChangeEvent<HTMLInputElement>): void {
+    const apCount = countApostrophes(e.target.value);
+    const newAcceptState = this.getNewAcceptStateOnChange(apCount);
+    if (newAcceptState !== this.state.acceptState) {
+      this.setState({acceptState: newAcceptState});
+    }
+    const newValue = getPrefixedValue(e.target.value, newAcceptState, apCount > 0);
+    if (this.props.value !== newValue) {
+      this.props.onChange(newValue);
+    }
+  }
+
+  @autobind
+  // tslint:disable-next-line:typedef
   private onDeclineBtnClickHandler() {
     this.setState({acceptState: AcceptState.NOT_ACCEPTED});
+  }
+
+  @autobind
+  private onFocus(): void {
+    this.setState({focused: true});
   }
 }
 
@@ -145,13 +153,16 @@ export enum AcceptState {
 }
 
 // Never returns ACCEPTED prefix, cause string value never contains such a prefix.
-function getAcceptStateFromPrefix(s: string): AcceptState | null {
+function getAcceptStateFromPrefix(s: string): AcceptState | null  {
   if(s) {
     const matches = s.match(PREFIX_REGEX);
     if(matches !== null) {
+      // tslint:disable-next-line:ban-ts-ignore
+      // @ts-ignore
       return AcceptState[matches[0]];
     }
   }
+
   return null;
 }
 
@@ -166,6 +177,7 @@ function getPrefixedValue(value: string, acceptState: AcceptState, hasApostrophe
   if (isPrefixNeeded(value, acceptState, hasApostrophe)) {
     return addPrefix(cleanValue, acceptState);
   }
+
   return cleanValue;
 }
 
@@ -173,6 +185,7 @@ function addPrefix(s: string, prefix: string): string {
   if (prefix === null) {
     return s;
   }
+
   return `${prefix}${s}`;
 }
 
@@ -180,13 +193,17 @@ function dropPrefix(s: string): string {
   if (s) {
     return s.replace(PREFIX_REGEX, '');
   }
+
   return s;
 }
 
+// tslint:disable-next-line:typedef
 function countApostrophes(s: string) {
-  let ap_match = s.match(APOSTROPHE_REGEX);
+  // tslint:disable-next-line:variable-name
+  const ap_match = s.match(APOSTROPHE_REGEX);
   if (ap_match === null) {
     return 0;
+    // tslint:disable-next-line:unnecessary-else
   } else {
     return ap_match.length;
   }
