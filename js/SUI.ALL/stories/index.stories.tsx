7021da545521fdb088665f31dce4298818eee508
el1191 <el1191@woyd.ru>
2020-03-03 14:49:15 +0300

    Fix styles + stories

--- js/SUI.ALL/stories/index.stories.tsx
+++ js/SUI.ALL/stories/index.stories.tsx
@@ -1,11 +1,15 @@
 import { storiesOf } from '@storybook/react';
+import { Button, DatePicker } from 'antd';
 import * as React from 'react';
 
 import { TooltipIcon } from '../src/lib/TooltipIcon';
-
 // tslint:disable-next-line:no-import-side-effect
 import '../styles/index.less';
 
 
 storiesOf('Button', module)
-  .add('with text', () => <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}><TooltipIcon>Hello Button</TooltipIcon></div>);
+  .add('with text', () => <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+    <Button>ad</Button>
+    <DatePicker.RangePicker/>
+    <TooltipIcon>Hello Button</TooltipIcon>
+  </div>);
