919456421e470d4e8ed34ab519bddab908aa0e92
HashAlexey <tiapkinalexey@gmail.com>
2020-03-10 17:42:32 +0300

    CustomFioInput test

--- js/SUI.ALL/src/lib/Inputs/CustomFioInput.tsx
+++ js/SUI.ALL/src/lib/Inputs/CustomFioInput.tsx
@@ -1,20 +1,62 @@
 /* tslint:disable */
 import {Input} from 'antd';
 import {InputProps} from 'antd/lib/input';
+import autobind from "autobind-decorator";
 import React from "react";
 import {fioConverterWithoutTrim} from "../validator";
+
+function convertValue(value?: string): string | null | undefined {
+  return value && fioConverterWithoutTrim(value);
+}
+
 export type CustomFioInputProps = InputProps & {
 }
 
-export class CustomFioInput extends React.Component<CustomFioInputProps> {
+export type CustomFioInputState = {
+  value?: string;
+}
+
+export class CustomFioInput extends React.Component<CustomFioInputProps, CustomFioInputState> {
+
+  public constructor(props: CustomFioInputProps) {
+    super(props);
+    this.state = {value: convertValue(props.value as string)}
+  }
+
+  public componentDidMount(): void {
+    this.doOnDid();
+  }
+
+  public componentDidUpdate(): void {
+    this.doOnDid();
+  }
 
   public render(): React.ReactNode {
     return (
       <Input
         {...this.props}
-        value={this.props.value && fioConverterWithoutTrim(this.props.value as string)}
+        value={this.state.value}
+        onChange={this.onChange}
       />
     );
+  }
+
+  @autobind
+  private doOnDid(): void {
+    if (this.props.value !== this.state.value) {
+      this.props.onChange({target: {value: this.state.value}} as any)
+    }
+  }
+
+  @autobind
+  private onChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    this.props.onChange({
+      ...event,
+      target: {
+        ...event.target,
+        value: convertValue(event.target.value)
+      }
+    });
   }
 
 }
