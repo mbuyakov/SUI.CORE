2357ec4c3aacae55201331eff8d758baad5b188c
el1191 <el1191@woyd.ru>
2020-03-18 18:22:09 +0300

    Fixes

--- js/SUI.ALL/src/lib/Inputs/PromisedMaterialIconButton.tsx
+++ js/SUI.ALL/src/lib/Inputs/PromisedMaterialIconButton.tsx
@@ -1,23 +1,23 @@
 import { CircularProgress, IconButton, Tooltip } from '@material-ui/core';
+import { ExtendButtonBaseTypeMap } from '@material-ui/core/ButtonBase/ButtonBase';
+import { IconButtonTypeMap } from '@material-ui/core/IconButton/IconButton';
+import { OverrideProps } from '@material-ui/core/OverridableComponent';
 import * as React from 'react';
 
 import { IPromisedBaseState, PromisedBase } from './PromisedBase';
 
-export class PromisedMaterialIconButton extends PromisedBase<{
-  disabled?: boolean;
+export class PromisedMaterialIconButton extends PromisedBase<{ href: string } & OverrideProps<ExtendButtonBaseTypeMap<IconButtonTypeMap>, 'a'> & {
   icon?: JSX.Element;
   loading?: boolean;
   progressColor?: 'primary' | 'secondary';
-  size?: 'small' | 'medium';
   tooltipText?: string;
-  type?: 'primary' | 'secondary';
 },
   IPromisedBaseState<{}>,
   {}> {
 
   public render(): JSX.Element {
     let btn = (
-      <IconButton onClick={this.saveWithoutValue} size={this.props.size} disabled={this.props.disabled} color={this.props.type}>
+      <IconButton onClick={this.saveWithoutValue} {...this.props}>
         {(this.props.loading || this.state.loading) ? <CircularProgress size={this.props.size === 'small' ? 16 : 24} color={this.props.progressColor}/> : (this.props.children || this.props.icon)}
       </IconButton>
     );
