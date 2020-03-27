a081f90125a630a67d7bf76a2ba22d69c3fce0c3
el1191 <el1191@woyd.ru>
2020-03-27 14:04:37 +0300

    Add IMaterialIconModalLinkProps

--- js/SUI.ALL/src/lib/Link/index.ts
+++ js/SUI.ALL/src/lib/Link/index.ts
@@ -1,5 +1,6 @@
 export * from "./ExternalLink";
 export * from "./MetaLink";
+export * from "./MaterialIconModalLink";
 export * from "./ModalLink";
 export * from "./RouterLink";
 export * from "./RouterLinkCellRender";
