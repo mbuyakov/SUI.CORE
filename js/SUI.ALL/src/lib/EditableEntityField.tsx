4291104f95f082faae1c08055aecbf88e91acda0
Maksim Khlystov <maxvls@gmail.com>
2020-04-28 19:15:42 +0300

    Fix format multiline string for graphql

--- js/SUI.ALL/src/lib/EditableEntityField.tsx
+++ js/SUI.ALL/src/lib/EditableEntityField.tsx
@@ -1,12 +1,13 @@
-import { notification } from 'antd';
+import {notification} from 'antd';
 import * as React from 'react';
 
-import { NO_DATA_TEXT } from './const';
-import { errorNotification } from './drawUtils';
-import { generateUpdate, PossibleId, PossibleValue } from './gql/queryGenerator';
+import {NO_DATA_TEXT} from './const';
+import {errorNotification} from './drawUtils';
+import {generateUpdate, PossibleId, PossibleValue} from './gql/queryGenerator';
 import {EditablePromisedComponent, IEditablePromisedComponentProps, IPromisedBaseProps} from './Inputs';
 import {ExtractProps, IObjectWithIndex} from './other';
-import { WaitData } from './WaitData';
+import {WaitData} from './WaitData';
+import {formatRawForGraphQL} from "./stringFormatters";
 
 export interface IEditableEntityFieldProps<T> extends Omit<IEditablePromisedComponentProps<T>, "children"> {
   children: React.ReactElement<Omit<IPromisedBaseProps<T>, 'promise'>>;
@@ -16,9 +17,13 @@ export interface IEditableEntityFieldProps<T> extends Omit<IEditablePromisedComp
   field: string;
   id: PossibleId;
   successMessage: string;
+
   afterSave?(): Promise<void>;
+
   customDefaultValue?(value: T): any;
+
   selectValueGenerator?(data: any): JSX.Element[];
+
   valuePreSaveConverter?(value: T): PossibleValue;
 }
 
@@ -27,7 +32,7 @@ export class EditableEntityField<T = any> extends React.Component<IEditableEntit
 
   public render(): JSX.Element {
     return (
-      <div style={{ display: 'inline-block' }}>
+      <div style={{display: 'inline-block'}}>
         <WaitData<IObjectWithIndex>
           ref={this.waitDataRef}
           alwaysUpdate={true}
@@ -52,19 +57,25 @@ export class EditableEntityField<T = any> extends React.Component<IEditableEntit
                 defaultChecked: this.props.customDefaultValue ? this.props.customDefaultValue(this.props.customQuery ? data : data[this.props.field]) : data[this.props.field],
                 defaultValue: this.props.customDefaultValue ? this.props.customDefaultValue(this.props.customQuery ? data : data[this.props.field]) : data[this.props.field],
                 promise: async (newValue: any) => {
-                  if (this.props.valuePreSaveConverter) {
-newValue = this.props.valuePreSaveConverter(newValue);
-                  }
+                  let valueForUpdate = this.props.valuePreSaveConverter
+                    ? this.props.valuePreSaveConverter(newValue)
+                    : newValue;
 
-                  let update = generateUpdate(this.props.entity, this.props.id, this.props.field, typeof (newValue) === 'string' ? newValue.replace(/"/g, '\\"') : newValue === undefined ? null : newValue).then((): void => {
-                    notification.success({ message: this.props.successMessage });
-                    if (this.waitDataRef.current) {
-this.waitDataRef.current.updateData();
-                    }
-                  }).catch(reason => {
-                    errorNotification(this.props.failMessage, reason.stack);
-                    throw reason;
-                  });
+                  valueForUpdate = typeof (valueForUpdate) === 'string'
+                    ? formatRawForGraphQL(valueForUpdate)
+                    : valueForUpdate === undefined
+                      ? null
+                      : valueForUpdate;
+                  let update = generateUpdate(this.props.entity, this.props.id, this.props.field, valueForUpdate)
+                    .then((): void => {
+                      notification.success({message: this.props.successMessage});
+                      if (this.waitDataRef.current) {
+                        this.waitDataRef.current.updateData();
+                      }
+                    }).catch(reason => {
+                      errorNotification(this.props.failMessage, reason.stack);
+                      throw reason;
+                    });
 
                   if (this.props.afterSave) {
                     update = update.then(this.props.afterSave);
