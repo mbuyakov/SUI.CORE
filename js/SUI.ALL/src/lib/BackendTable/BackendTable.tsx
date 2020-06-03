51c2d2ef1b2cea72dc81079e3b40e4d156371a15
HashAlexey <tiapkinalexey@gmail.com>
2020-06-03 17:29:15 +0300

    [BackendTable] pageSizes [10, 25, 50] -> [10, 25, 50, 100, 250, 500, 1000]

--- js/SUI.ALL/src/lib/BackendTable/BackendTable.tsx
+++ js/SUI.ALL/src/lib/BackendTable/BackendTable.tsx
@@ -293,7 +293,7 @@ export class BackendTable<TSelection = defaultSelection>
           ].filter(Boolean)}
           rowStyler={this.generateRowStyler()}
           warnings={admin ? this.state.warnings : undefined}
-          pageSizes={[10, 25, 50]}
+          pageSizes={[10, 25, 50, 100, 250, 500, 1000]}
           defaultCurrentPage={this.state.defaultCurrentPage}
           // remote functions
           getChildGroups={this.getChildGroups}
