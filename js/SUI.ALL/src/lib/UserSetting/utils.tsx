84f142d55fb0f86a74cd95d6f4328f12d2784bfc
HashAlexey <tiapkinalexey@gmail.com>
2020-07-20 17:51:56 +0300

    Add utils function to UserSetting

--- js/SUI.ALL/src/lib/UserSetting/utils.tsx
+++ js/SUI.ALL/src/lib/UserSetting/utils.tsx
@@ -1,6 +1,45 @@
+import {AxiosPromise, AxiosResponse} from "axios";
 import * as React from "react";
 import {errorNotification} from "../drawUtils";
 import {IObjectWithIndex} from "../other";
+import {getUser} from "../utils";
+import axios from "axios";
+
+
+export function doHandledUserRestRequest<T, R = void>(
+  uri: string,
+  onSuccess: (response: AxiosResponse<R>) => void | Promise<void>,
+  body?: T,
+  headers?: IObjectWithIndex,
+  reThrowError: string | boolean = "Ошибка сохранения"
+): Promise<void> {
+  return doUserRestRequest<T, R>(uri, body, headers)
+    .then(onSuccess)
+    .catch((reason: any) => {
+      handleUserRestError(reason);
+      if (reThrowError) {
+        throw typeof(reThrowError) === "string" ? reThrowError : reason;
+      }
+    });
+}
+
+export function doUserRestRequest<T, R = void>(
+  uri: string,
+  body?: T,
+  headers?: IObjectWithIndex,
+): AxiosPromise<R> {
+  return axios.post<R>(
+    uri,
+    body,
+    {
+      headers: {
+        authorization: `Bearer ${getUser().accessToken}`,
+        "content-type": "application/json",
+        ...headers
+      }
+    }
+  );
+}
 
 export function handleUserRestError(reason: any, message?: string): void {
   const {data} = reason.response;
