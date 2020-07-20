f9683e2139f148111bfbfd1829e92bc478f9672e
HashAlexey <tiapkinalexey@gmail.com>
2020-07-20 14:58:22 +0300

    Rename folder User -> UserSetting (casing issue)

--- js/SUI.ALL/src/lib/index.ts
+++ js/SUI.ALL/src/lib/index.ts
@@ -57,5 +57,5 @@ export * from "./TableSettings";
 export * from "./TooltipIcon";
 export * from "./TransitionBar";
 export * from "./user";
-export * from "./User";
+export * from "./UserSetting";
 export * from "./WaitData";
