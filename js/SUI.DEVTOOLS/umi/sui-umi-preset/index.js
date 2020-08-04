c1bc1255587b3cf34cf2ae03aab4d6fc65ef84c9
el1191 <el1191@woyd.ru>
2020-08-04 14:40:57 +0300

    Fix package name in umi preset

--- js/SUI.DEVTOOLS/umi/sui-umi-preset/index.js
+++ js/SUI.DEVTOOLS/umi/sui-umi-preset/index.js
@@ -19,12 +19,12 @@ exports.default = () => {
       // require.resolve('@umijs/plugin-test'),
       require.resolve('@umijs/plugin-helmet'),
       ...(process.env.WEBPACK_FIVE ? [require.resolve("@umijs/plugin-webpack-5")] : []),
-      require.resolve("@sui/sui-builder/umi/plugin-antd-custom/lib"),
-      require.resolve("@sui/sui-builder/umi/sui-build-time"),
-      require.resolve("@sui/sui-builder/umi/sui-chunks"),
-      require.resolve("@sui/sui-builder/umi/sui-history-hash"),
-      require.resolve("@sui/sui-builder/umi/sui-kludges"),
-      require.resolve("@sui/sui-builder/umi/sui-theme"),
+      require.resolve("@sui/sui-devtools/umi/plugin-antd-custom/lib"),
+      require.resolve("@sui/sui-devtools/umi/sui-build-time"),
+      require.resolve("@sui/sui-devtools/umi/sui-chunks"),
+      require.resolve("@sui/sui-devtools/umi/sui-history-hash"),
+      require.resolve("@sui/sui-devtools/umi/sui-kludges"),
+      require.resolve("@sui/sui-devtools/umi/sui-theme"),
     ]
   };
 }
