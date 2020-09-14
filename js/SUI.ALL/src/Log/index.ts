7b82ddd3674cd163954be84885b7bbce927e5f4c
el1191 <el1191@woyd.ru>
2020-09-14 13:27:14 +0300

    Add OldAuditTable

--- js/SUI.ALL/src/Log/index.ts
+++ js/SUI.ALL/src/Log/index.ts
@@ -1,5 +1,6 @@
 export * from "./AuditLog";
 export * from "./AuditLogTable";
 export * from "./AuthenticationLogTable";
+export * from "./OldAuditTable";
 export * from "./TableExportLogTable";
 export * from "./utils";
