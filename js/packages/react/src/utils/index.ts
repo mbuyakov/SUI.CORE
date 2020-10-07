bcc0460fe649c62c8daef5adc195adfedce90cb6
el1191 <el1191@woyd.ru>
2020-10-07 12:59:00 +0300

    Add file saver for cordova

--- js/packages/react/src/utils/index.ts
+++ js/packages/react/src/utils/index.ts
@@ -1,5 +1,7 @@
-import { Container } from 'typescript-ioc';
-import { ICoreUser, UserService, getDataByKey } from '@sui/core';
+/* eslint-disable no-console,@typescript-eslint/no-explicit-any */
+import {Container} from 'typescript-ioc';
+import {getDataByKey, ICoreUser, IObjectWithIndex, UserService} from '@sui/core';
+import {notification} from "antd";
 
 
 export * from "./actionType";
@@ -16,12 +18,59 @@ export * from "./metaUtils";
 export * from "./TabSyncer";
 export * from "./versionMismatch";
 
+
+declare global {
+  const cordova: any;
+}
+
+function errorWhileSaving(e: Error | string | IObjectWithIndex): void {
+  notification.error({
+    message: "Ошибка при сохранении файла",
+    description: e instanceof Error ? e.message : (typeof e === "string" ? e : JSON.stringify(e))
+  })
+}
+
+
+function createFile(dirEntry: any, blob: Blob, fileName: string): void {
+  // Creates a new file
+  dirEntry.getFile(fileName, {create: true, exclusive: false}, (fileEntry) => {
+    writeFile(fileEntry, blob, fileName);
+  }, errorWhileSaving);
+}
+
+function writeFile(fileEntry: any, blob: Blob, fileName: string): void {
+  // Create a FileWriter object for our FileEntry
+  fileEntry.createWriter(fileWriter => {
+    fileWriter.onerror = errorWhileSaving;
+    fileWriter.onwriteend = (): void => {
+      notification.success({
+        message: "Файл сохранён",
+        description: `Файл с именем ${fileName} успешно добавлен в папку "Загрузки"`
+      })
+    }
+    fileWriter.write(blob);
+  })
+}
+
+function saveBlob2File(blob: Blob, fileName: string): void {
+  const folder = `${cordova.file.externalRootDirectory}Download`;
+  // @ts-ignore
+  window.resolveLocalFileSystemURL(folder, (dirEntry) => {
+    console.log(`file system open: ${dirEntry.name}`);
+    createFile(dirEntry, blob, fileName);
+  }, errorWhileSaving)
+}
+
 export function downloadFile(file: Blob, fileName: string): void {
-  const element = document.createElement('a');
-  element.href = URL.createObjectURL(file);
-  element.download = fileName;
-  document.body.appendChild(element);
-  element.click();
+  if (typeof cordova !== 'undefined' && cordova.platformId !== "browser") {
+    saveBlob2File(file, fileName);
+  } else {
+    const element = document.createElement('a');
+    element.href = URL.createObjectURL(file);
+    element.download = fileName;
+    document.body.appendChild(element);
+    element.click();
+  }
 }
 
 export function getUser(): ICoreUser {
