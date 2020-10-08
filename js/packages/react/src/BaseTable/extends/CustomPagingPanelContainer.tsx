1da689946e3513baa0e0d9a27fbc4e42c9ca2dff
Maksim Khlystov <maxvls@gmail.com>
2020-10-08 15:24:58 +0300

    Add logic to CustomPagingContainer #SDU-470

--- js/packages/react/src/BaseTable/extends/CustomPagingPanelContainer.tsx
+++ js/packages/react/src/BaseTable/extends/CustomPagingPanelContainer.tsx
@@ -1,23 +1,28 @@
 import {PagingPanel} from '@devexpress/dx-react-grid-material-ui';
 import * as React from "react";
+import {ReactElement} from "react";
 import {Select} from "antd";
+import {Template} from "@devexpress/dx-react-core";
+import {PagingPanel as PagingPanelBase} from "@devexpress/dx-react-grid";
 
 type ICustomPagingContainer = PagingPanel.ContainerProps & { className?: string; style?: React.CSSProperties; [x: string]: any };
 
-export function CustomPagingPanelContainer(props: ICustomPagingContainer): JSX.Element {
+export function CustomPagingPanelContainer(props: ICustomPagingContainer): ReactElement<PagingPanelBase.ContainerProps> {
   return (
-    <div>
       <PagingPanel.Container
         {...props}
-      />
-      <span>Страница:</span>
-      <Select
-        onChange={props.onCurrentPageChange}
       >
-        {Array.from({length: props.totalPages}, (_, key) => (
-          <Select.Option key={key + 1} value={key + 1}>{key + 1}</Select.Option>
-        ))}
-      </Select>
-    </div>
+        <Template name="custom-page-selector">
+          <span>Страница:</span>
+          <Select
+            defaultValue={props.currentPage}
+            onChange={props.onCurrentPageChange}
+          >
+            {Array.from({length: props.totalPages}, (_, key) => (
+              <Select.Option key={key + 1} value={key + 1}>{key + 1}</Select.Option>
+            ))}
+          </Select>
+        </Template>
+      </PagingPanel.Container>
   );
 }
