a5cc4ab6054d7be89c92d4eaef6a4e8c0b0a9ea8
Maksim Khlystov <maxvls@gmail.com>
2020-10-09 14:15:56 +0300

    Use select or input box for small or big number of pages in CustomPagingContainer #SDU-470

--- js/packages/react/src/BaseTable/extends/CustomPagingPanelContainer.tsx
+++ js/packages/react/src/BaseTable/extends/CustomPagingPanelContainer.tsx
@@ -1,28 +1,77 @@
 import {PagingPanel} from '@devexpress/dx-react-grid-material-ui';
 import * as React from "react";
 import {ReactElement} from "react";
-import {Select} from "antd";
-import {Template} from "@devexpress/dx-react-core";
+import Select from '@material-ui/core/Select';
 import {PagingPanel as PagingPanelBase} from "@devexpress/dx-react-grid";
+import {Input} from "@material-ui/core";
+import debounce from "lodash/debounce";
 
-type ICustomPagingContainer = PagingPanel.ContainerProps & { className?: string; style?: React.CSSProperties; [x: string]: any };
+const DEBOUNCE_ON_CHANGE_MS = 1000;
+const MAX_NUMBER_TO_SELECT_IN_MENU = 20;
+
+export type ICustomPagingContainer = PagingPanel.ContainerProps & { className?: string; style?: React.CSSProperties; [x: string]: any };
 
 export function CustomPagingPanelContainer(props: ICustomPagingContainer): ReactElement<PagingPanelBase.ContainerProps> {
+  const debouncedOnChange = props.onCurrentPageChange && debounce(props.onCurrentPageChange, DEBOUNCE_ON_CHANGE_MS);
+  const input = props.totalPages <= MAX_NUMBER_TO_SELECT_IN_MENU
+    ? (
+      <Select
+        native
+        defaultValue={props.currentPage}
+        disableUnderline={true}
+        onChange={event => props.onCurrentPageChange && props.onCurrentPageChange(event.target.value as number || 0)}
+      >
+        {Array.from({length: props.totalPages}, (_, key) => (
+          <option value={key}>{key + 1}</option>
+        ))}
+      </Select>
+    )
+    : (
+      <Input
+        defaultValue={props.currentPage + 1}
+        disableUnderline
+        inputProps={{
+          min: 1,
+          max: props.totalPages,
+          type: "number",
+        }}
+        onChange={event => {
+          if(!!debouncedOnChange) {
+            const page = event.target.value && parseInt(event.target.value, 10);
+            debouncedOnChange((!page || isNaN(page)) ? 0 : page - 1);
+          }
+        }}
+        style={{
+          border: "none",
+          fontSize: "14px",
+          fontFamily: "Roboto, Helvetica, Arial, sans-serif",
+          fontWeight: 400,
+          lineHeight: "1.1876em",
+          letterSpacing: "0.00938em",
+        }}
+        type="number"
+      />
+    );
+
   return (
+    <div style={{display: "flex", justifyContent: "flex-end"}}>
       <PagingPanel.Container
         {...props}
-      >
-        <Template name="custom-page-selector">
-          <span>Страница:</span>
-          <Select
-            defaultValue={props.currentPage}
-            onChange={props.onCurrentPageChange}
-          >
-            {Array.from({length: props.totalPages}, (_, key) => (
-              <Select.Option key={key + 1} value={key + 1}>{key + 1}</Select.Option>
-            ))}
-          </Select>
-        </Template>
-      </PagingPanel.Container>
+      />
+      <div style={{flex: "none", display: "flex", padding: "12px", overflow: "hidden", alignItems: "center", justifyContent: "flex-end"}}>
+        <span style={{
+          fontSize: "0.75rem",
+          fontFamily: "Roboto, Helvetica, Arial, sans-serif",
+          fontWeight: 400,
+          lineHeight: 1.66,
+          letterSpacing: "0.03333em",
+          paddingRight: "24px",
+        }}
+        >
+          Страница
+        </span>
+        {input}
+      </div>
+    </div>
   );
 }
