375f3ba199b603e4d7e0850d5d1662175b159136
HashAlexey <tiapkinalexey@gmail.com>
2020-10-13 19:10:40 +0300

    Remove EnableAutoConfiguration from ru.sui.utils.kotlin.config.AutoConfiguration

--- java/sui-utils/sui-utils-kotlin/src/main/java/ru/sui/utils/kotlin/config/AutoConfiguration.kt
+++ java/sui-utils/sui-utils-kotlin/src/main/java/ru/sui/utils/kotlin/config/AutoConfiguration.kt
@@ -1,14 +1,9 @@
 package ru.sui.utils.kotlin.config
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 
 @Configuration
 @ComponentScan("ru.sui.utils.kotlin")
-// Костыль, чтобы резолвить контекст в sui проектах
-// Находится тут, т.к. sui-utils-kotlin есть во всех других sui-проектах
-// Не влияет на работоспособность sui, т.к. EnableAutoConfiguration является частью SpringBootApplication
-@EnableAutoConfiguration
 class AutoConfiguration
