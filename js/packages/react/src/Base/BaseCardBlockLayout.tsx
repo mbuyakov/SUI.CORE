0d5103618e16303daf7da0051aafca4113be90ee
el1191 <el1191@woyd.ru>
2020-10-15 01:52:28 +0300

    Disable padding for collapse panels in block

--- js/packages/react/src/Base/BaseCardBlockLayout.tsx
+++ js/packages/react/src/Base/BaseCardBlockLayout.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {Card} from 'antd';
 import {OneOrArrayWithNulls, wrapInArrayWithoutNulls} from '@sui/core';
-import {BaseCardRow, IBaseCardRowLayout} from "@/Base/BaseCardRow";
+import {BaseCardRow, IBaseCardRowLayout, isRowWithCollapse} from "@/Base/BaseCardRow";
 
 export interface IBaseCardBlockLayout<T, ITEM> {
   rows: OneOrArrayWithNulls<IBaseCardRowLayout<T, ITEM>>;
@@ -10,18 +10,22 @@ export interface IBaseCardBlockLayout<T, ITEM> {
 
 export const BaseCardBlock: <T, ITEM>(props: IBaseCardBlockLayout<T, ITEM> & {
   sourceItem: T
-}) => JSX.Element = props => (
-  <Card title={props.title} style={{margin: "12px 0"}}>
-    {wrapInArrayWithoutNulls(props.rows).map((row, index, arr) => (
-      <BaseCardRow
-        key={index.toString()}
-        sourceItem={props.sourceItem}
-        row={row}
-        rowIndex={index}
-        parent="block"
-        rowsLength={arr.length}
-      />
-    ))}
-  </Card>
-);
+}) => JSX.Element = props => {
+  const rows = wrapInArrayWithoutNulls(props.rows);
+  const noPadding = rows.length == 1 && isRowWithCollapse(rows[0]); // If block have only collapsePanels - disable padding
+  return (
+    <Card title={props.title} style={{margin: "12px 0"}} bodyStyle={noPadding ? {padding: 0} : {}}>
+      {rows.map((row, index, arr) => (
+        <BaseCardRow
+          key={index.toString()}
+          sourceItem={props.sourceItem}
+          row={row}
+          rowIndex={index}
+          parent="block"
+          rowsLength={arr.length}
+        />
+      ))}
+    </Card>
+  );
+}
 
