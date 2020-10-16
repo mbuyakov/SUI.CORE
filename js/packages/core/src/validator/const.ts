ac615547839f3369f986f6efcc289e3df73bd2c2
Nikita Kostin <n.kostin11@mail.ru>
2020-10-16 15:03:16 +0300

    Remove issued by regex, desc and department code to core

--- js/packages/core/src/validator/const.ts
+++ js/packages/core/src/validator/const.ts
@@ -20,3 +20,9 @@ export const ORGANIZATION_INN_MASK_LENGTH = 10;
 
 export const OGRN_MASK = "1 11 11 1111111 1";
 export const OGRN_MASK_LENGTH = 13;
+
+export const ISSUED_BY_REGEX = "^[А-Яа-я\\s№.\\-]{1,250}$";
+export const ISSUED_BY_DESC = "до 250 знаков";
+
+export const DEPARTMENT_CODE_REGEX = "^[0-9]{3}\\-[0-9]{3}$";
+export const DEPARTMENT_CODE_DESC = "7 знаков, NNN-NNN, где N - число";
