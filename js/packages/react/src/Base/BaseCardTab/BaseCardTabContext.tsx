06e52c92881d11d975887a7d9caa8122e4367882
el1191 <el1191@woyd.ru>
2020-11-03 03:45:11 +0300

    Add tab with url sync + move current tab state to BaseCardTabContext

--- js/packages/react/src/Base/BaseCardTab/BaseCardTabContext.tsx
+++ js/packages/react/src/Base/BaseCardTab/BaseCardTabContext.tsx
@@ -1,4 +1,55 @@
 import * as React from 'react';
+import {generatePath, RouteComponentProps, withRouter} from "react-router-dom";
+import autobind from 'autobind-decorator';
 
-// @ts-ignore
-export const BaseCardTabContext = React.createContext<(tab: string) => void>(null);
+const BaseCardTabContext = React.createContext<{ setTab(tab: string): void, tab: string }>(null);
+
+type BaseCardTabContextProps = RouteComponentProps & {
+  routeKey?: string;
+};
+
+class BaseCardTabContextProviderInner extends React.Component<BaseCardTabContextProps, {
+  tab: string
+}> {
+  public constructor(props: BaseCardTabContextProps) {
+    super(props);
+    let tab = "0";
+    if (props.routeKey) {
+      // Backward compatibility
+      if (props.match.params[props.routeKey] == null) {
+        props.match.params[props.routeKey] = tab;
+        props.history.replace(generatePath(props.match.path, props.match.params));
+      } else {
+        tab = props.match.params[props.routeKey];
+      }
+    }
+
+    this.state = {
+      tab
+    };
+  }
+
+  public render(): JSX.Element {
+    return (
+      <BaseCardTabContext.Provider value={{setTab: this.setTab, tab: this.state.tab}}>
+        {this.props.children}
+      </BaseCardTabContext.Provider>
+    );
+  }
+
+  @autobind
+  public setTab(tab: string): void {
+    this.setState({tab});
+    this.props.match.params[this.props.routeKey] = tab;
+    this.props.history.replace(generatePath(this.props.match.path, this.props.match.params));
+  }
+
+  public componentDidUpdate(): void {
+    if (this.props.routeKey && this.props.match.params[this.props.routeKey] != this.state.tab) {
+      this.setTab(this.props.match.params[this.props.routeKey]);
+    }
+  }
+}
+
+export const BaseCardTabContextProvider = withRouter(BaseCardTabContextProviderInner);
+export const BaseCardTabContextConsumer = BaseCardTabContext.Consumer;
