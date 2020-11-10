172850aeef2abd046f547e21313abae497b50ef5
el1191 <el1191@woyd.ru>
2020-11-10 14:40:46 +0300

    Add dark theme support for amcharts

--- js/packages/charts/src/_amcharts.ts
+++ js/packages/charts/src/_amcharts.ts
@@ -4,6 +4,7 @@ export interface AMCHARTS {
   am4charts: typeof import('@amcharts/amcharts4/charts'),
   am4lang_ru_RU: typeof import('@amcharts/amcharts4/lang/ru_RU').default,
   am4themes_animated: typeof import('@amcharts/amcharts4/themes/animated').default,
+  am4themes_dark: typeof import('@amcharts/amcharts4/themes/dark').default,
   am4maps: typeof import('@amcharts/amcharts4/maps'),
   am4forceDirected: typeof import('@amcharts/amcharts4/plugins/forceDirected'),
   am4wordCloud: typeof import('@amcharts/amcharts4/plugins/wordCloud')
@@ -26,6 +27,7 @@ export async function getAmcharts(): Promise<AMCHARTS> {
       am4charts,
       am4lang_ru_RU,
       am4themes_animated,
+      am4themes_dark,
       am4maps,
       am4forceDirected,
       am4wordCloud
@@ -34,6 +36,7 @@ export async function getAmcharts(): Promise<AMCHARTS> {
       import('@amcharts/amcharts4/charts'),
       import('@amcharts/amcharts4/lang/ru_RU'),
       import('@amcharts/amcharts4/themes/animated'),
+      import('@amcharts/amcharts4/themes/dark'),
       import('@amcharts/amcharts4/maps'),
       import('@amcharts/amcharts4/plugins/forceDirected'),
       import('@amcharts/amcharts4/plugins/wordCloud')
@@ -47,6 +50,7 @@ export async function getAmcharts(): Promise<AMCHARTS> {
       am4charts,
       am4lang_ru_RU: am4lang_ru_RU.default,
       am4themes_animated: am4themes_animated.default,
+      am4themes_dark: am4themes_dark.default,
       am4maps,
       am4forceDirected,
       am4wordCloud
