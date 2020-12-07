ca3e64a9e6637da4acb43765adcc60f1a668353e
el1191 <el1191@woyd.ru>
2020-12-07 10:58:04 +0300

    Refactor dateValidators

--- js/packages/core/src/validator/utils.ts
+++ js/packages/core/src/validator/utils.ts
@@ -1,18 +1,18 @@
 import moment, {Moment} from "moment";
 
-export function dateDisabler(bound: string, type: "greater" | "greaterOrEqual" | "less" | "lessOrEqual"): (current: Moment) => boolean {
-  return (current: Moment): boolean => {
+export function dateDisabler(bound: string| Moment, type: "greater" | "greaterOrEqual" | "less" | "lessOrEqual"): (target: Moment) => boolean {
+  return (target: Moment): boolean => {
     const boundMoment = moment(bound);
 
     switch (type) {
       case "greater":
-        return boundMoment.endOf("day") < current;
+        return boundMoment.endOf("day") < target;
       case "greaterOrEqual":
-        return boundMoment.startOf("day") <= current;
+        return boundMoment.startOf("day") <= target;
       case "less":
-        return boundMoment.startOf("day") > current;
+        return boundMoment.startOf("day") > target;
       case "lessOrEqual":
-        return boundMoment.endOf("day") >= current;
+        return boundMoment.endOf("day") >= target;
     }
 
     return false;
