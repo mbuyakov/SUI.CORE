2f05a501e1828384af3a480eb48040f1525f92f6
HashAlexey <tiapkinalexey@gmail.com>
2020-12-28 17:55:21 +0300

    [Experimental] SelectAll + SelectionFilter

--- js/packages/react/src/BaseTable/extends/index.ts
+++ js/packages/react/src/BaseTable/extends/index.ts
@@ -5,6 +5,7 @@ export * from "./DoubleScrollbar";
 export * from "./EmptyMessageComponent";
 export * from "./ExportPlugin";
 export * from "./GroupSummaryRow";
+export * from "./IntegratedSelection";
 export * from "./TableNoDataCell";
 export * from "./ToolbarBase";
 export * from "./UserSettingsPlugin";
