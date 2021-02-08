8fd87bb7a4772f20dfb4b21a92a99e92ca951adc
HashAlexey <tiapkinalexey@gmail.com>
2021-02-08 17:42:55 +0300

    DoubleScrollbar fixes

--- js/packages/react/src/BaseTable/extends/DoubleScrollbar.tsx
+++ js/packages/react/src/BaseTable/extends/DoubleScrollbar.tsx
@@ -1,53 +1,49 @@
 import autobind from "autobind-decorator";
 import * as React from 'react';
 
-interface IDoubleScrollbarState {
-  width: React.CSSProperties["width"];
-}
-
 // fixed https://github.com/umchee/react-double-scrollbar/blob/master/src/DoubleScrollbar.jsx
-export class DoubleScrollbar extends React.Component<{}, IDoubleScrollbarState> {
+export class DoubleScrollbar extends React.Component {
 
   private childrenWrapper: React.RefObject<HTMLDivElement> = React.createRef();
   private outerDiv: React.RefObject<HTMLDivElement> = React.createRef();
-
-  public constructor() {
-    super({});
-    this.state = {width: "auto"}
-  }
 
   public componentDidMount(): void {
     const outerDiv = this.outerDiv.current;
     const childWrapper = this.childrenWrapper.current;
 
-    // Set initial width
-    this.calculateWidth();
-
     // Update width when window size changes
-    window.addEventListener("resize", this.calculateWidth);
+    window.addEventListener("resize", this.update);
 
     // assoc the scrolls
-    outerDiv.onscroll = function () {
+    outerDiv.onscroll = function(): void {
       childWrapper.scrollLeft = outerDiv.scrollLeft;
     };
 
-    childWrapper.onscroll = function () {
+    childWrapper.onscroll = function(): void {
       outerDiv.scrollLeft = childWrapper.scrollLeft;
     };
+
+    // Set initial width
+    this.update();
+  }
+
+  public componentDidUpdate(prevProps: Readonly<{}>): void {
+    // Н - Надежность
+    if (prevProps !== this.props) {
+      this.forceUpdate();
+    }
   }
 
   public componentWillUnmount(): void {
-    window.removeEventListener("resize", this.calculateWidth);
-  }
-
-  public componentDidUpdate(): void {
-    this.calculateWidth();
+    window.removeEventListener("resize", this.update);
   }
 
   public render(): JSX.Element {
-    let outerDivStyle: React.CSSProperties = {marginTop: -1, overflowX: "auto", overflowY: "hidden"};
-    let innerDivStyle: React.CSSProperties = {height: 1, width: this.state.width};
-    let childDivStyle: React.CSSProperties = {overflow: "auto", overflowY: "hidden"};
+    const width = this.getChildWrapperWidth() || "auto";
+
+    const outerDivStyle: React.CSSProperties = {marginTop: -1, overflowX: "auto", overflowY: "hidden"};
+    const innerDivStyle: React.CSSProperties = {height: 1, width};
+    const childDivStyle: React.CSSProperties = {overflow: "auto", overflowY: "hidden"};
 
     return (
       <div>
@@ -62,18 +58,6 @@ export class DoubleScrollbar extends React.Component<{}, IDoubleScrollbarState> 
   }
 
   @autobind
-  private calculateWidth(): void {
-    const width = this.getChildWrapperWidth() || "auto";
-
-    // Set the width of the inner div to the first child's
-    const widthNumber = Number(width.toString().replace("px", ""));
-    const stateWidthNumber = Number(width.toString().replace("px", ""));
-    if ((!isNaN(widthNumber) && !isNaN(stateWidthNumber)) ? (Math.abs(widthNumber - stateWidthNumber) > 5) : (width !== this.state.width)) {
-      this.setState({width});
-    }
-  }
-
-  @autobind
   private getChildWrapperWidth(): React.CSSProperties["width"] {
     let width = null;
 
@@ -82,6 +66,11 @@ export class DoubleScrollbar extends React.Component<{}, IDoubleScrollbarState> 
     }
 
     return width;
+  }
+
+  @autobind
+  private update(): void {
+    this.forceUpdate();
   }
 
 }
