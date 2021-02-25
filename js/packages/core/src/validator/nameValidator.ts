1ff7a468e55fa82e27724bad2a00f5d377372e07
Nikita Kostin <n.kostin11@mail.ru>
2021-02-25 16:18:10 +0300

    Remove english alphabet from nameValidator

--- js/packages/core/src/validator/nameValidator.ts
+++ js/packages/core/src/validator/nameValidator.ts
@@ -1,16 +1,14 @@
 import capitalize from 'lodash/capitalize';
 
-export const NAME_REGEXP = /^(?=(([А-ЯЁа-яёA-Za-z0-9().]+([\-'](?!\s|$))?|\s)+))\1$/;
-export const SURNAME_REGEXP = /^(?=(([А-ЯЁа-яёA-Za-z0-9()]+([\-'](?!\s|$))?|\s)+))\1$/;
+export const NAME_REGEXP = /^(?=(([А-ЯЁа-яё0-9().]+([\-'](?!\s|$))?|\s)+))\1$/;
+export const SURNAME_REGEXP = /^(?=(([А-ЯЁа-яё0-9()]+([\-'](?!\s|$))?|\s)+))\1$/;
 export const WHITESPACE_REGEXP = /^\s+.+$/;
-export const ONLY_ONE_ALPHABET_REGEXP = /^(?:[^А-ЯЁа-яё]*|[^A-Za-z]*)$/;
 export const ALL_SYMBOL_DIGIT_REGEXP = /^(\d*|[()]*)*$/;
 export const POINT_REGEXP_SURNAME_REGEXP = /(?:\s*\.\s*)/;
-export const NAME_REGEXP_MESSAGE = "Допустимые символы: А-Я,а-я,A-Z,a-z,0-9,(),-,' точка \".\" в Фамилии не допускается, знак \"-\" не может быть последним символом, идти подряд или через пробел";
+export const NAME_REGEXP_MESSAGE = "Допустимые символы: А-Я,а-я,0-9,(),-,' точка \".\" в Фамилии не допускается, знак \"-\" не может быть последним символом, идти подряд или через пробел";
 export const ALL_SYMBOL_MESSAGE = "Все символы не могут быть в верхнем/нижнем регистре";
 export const ALL_SYMBOL_DIGIT_MESSAGE = "Все символы не могут быть цифрами или скобками";
 export const WHITESPACE_MESSAGE = "Удалите лишние пробелы в начале";
-export const ONLY_ONE_ALPHABET_MESSAGE = "Введите все буквы в одной раскладке";
 export const POINT_REGEXP_SURNAME_MESSAGE = "Точка \".\" в Фамилии не допускается";
 
 export function nameValidator(field: "firstName" | "middleName" | "lastName", allowNulls?: boolean): (name: string | null) => string {
@@ -19,18 +17,14 @@ export function nameValidator(field: "firstName" | "middleName" | "lastName", al
     if (!name || !name.trim().length) {
       return (field === "middleName" || allowNulls) ? '' : "Поле должно быть заполнено";
     } else {
-      if (!ONLY_ONE_ALPHABET_REGEXP.test(name)) {
-        return ONLY_ONE_ALPHABET_MESSAGE;
+      if (field === 'lastName') {
+        return SURNAME_REGEXP.test(name)
+          ? allSymbolValidator(name)
+          : whiteSpaceAndPointValidator(name, field);
       } else {
-        if (field === 'lastName') {
-          return SURNAME_REGEXP.test(name)
-            ? allSymbolValidator(name)
-            : whiteSpaceAndPointValidator(name, field);
-        } else {
-          return NAME_REGEXP.test(name)
-            ? allSymbolValidator(name)
-            : whiteSpaceAndPointValidator(name, field);
-        }
+        return NAME_REGEXP.test(name)
+          ? allSymbolValidator(name)
+          : whiteSpaceAndPointValidator(name, field);
       }
     }
   }
