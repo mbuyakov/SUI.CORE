2b2e6d1c9033f844353aa13109811de5a6ce327f
Nikita Kostin <n.kostin11@mail.ru>
2021-03-18 12:37:14 +0300

    Add PersonUniqueFieldValidator

---
+++ js/packages/core/src/validator/PersonUniqueFieldValidator.tsx
@@ -0,0 +1,19 @@
+import {DBUniqueValidator} from "@/validator/DBUniqueValidator";
+
+export async function PersonUniqueFieldValidator<T = string>(fieldName: string,
+                                                             fieldValue: T,
+                                                             errMessage: string,
+                                                             excludePersonId: string = null,
+                                                             linkToEntity: (idFieldName: string, entityId: string, errMessage: string) => void): Promise<any> {
+  return new Promise((resolve, reject): any => {
+    DBUniqueValidator("allPeople", {fieldName: fieldValue, 'deleted': false}, 'id', excludePersonId)
+      .then(data => {
+        if (data.table.totalCount > 0) {
+          reject(linkToEntity('id', data.table.nodes[0].__id, errMessage));
+        } else {
+          resolve();
+        }
+      })
+      .catch(err => reject(`Ошибка чтения данных из БД. Попробуйте снова ввести значение\n${err}`));
+  });
+}
