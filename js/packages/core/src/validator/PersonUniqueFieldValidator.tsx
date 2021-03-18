05b3a15edb19128545d18bc23e2cf1a059cbd179
Nikita Kostin <n.kostin11@mail.ru>
2021-03-18 13:19:46 +0300

    Fix PersonUniqueFieldValidator №2

--- js/packages/core/src/validator/PersonUniqueFieldValidator.tsx
+++ js/packages/core/src/validator/PersonUniqueFieldValidator.tsx
@@ -5,7 +5,7 @@ export async function PersonUniqueFieldValidator<T = string>(fieldName: string,
                                                              excludePersonId: string = null,
                                                              linkToEntity: (idFieldName: string, entityId: string) => void): Promise<any> {
   return new Promise((resolve, reject): any => {
-    DBUniqueValidator("allPeople", {fieldName: fieldValue, 'deleted': false}, 'id', excludePersonId)
+    DBUniqueValidator("allPeople", {[fieldName]: fieldValue, 'deleted': false}, 'id', excludePersonId)
       .then(data => {
         if (data.table.totalCount > 0) {
           reject(linkToEntity('id', data.table.nodes[0].__id));
