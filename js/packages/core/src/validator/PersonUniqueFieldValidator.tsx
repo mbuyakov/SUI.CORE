e788bc226f191742e6a9136bdf86fe2f20085f2e
Nikita Kostin <n.kostin11@mail.ru>
2021-03-18 14:40:45 +0300

    Add errMessage to PersonUniqueFieldValidator

--- js/packages/core/src/validator/PersonUniqueFieldValidator.tsx
+++ js/packages/core/src/validator/PersonUniqueFieldValidator.tsx
@@ -2,13 +2,14 @@ import {DBUniqueValidator} from "@/validator/DBUniqueValidator";
 
 export async function PersonUniqueFieldValidator<T = string>(fieldName: string,
                                                              fieldValue: T,
+                                                             errMessage: string,
                                                              excludePersonId: string = null,
-                                                             linkToEntity: (idFieldName: string, entityId: string) => void): Promise<any> {
+                                                             linkToEntity: (idFieldName: string, entityId: string, errMessage: string) => void): Promise<any> {
   return new Promise((resolve, reject): any => {
     DBUniqueValidator("allPeople", {[fieldName]: fieldValue, 'deleted': false}, 'id', excludePersonId)
       .then(data => {
         if (data.table.totalCount > 0) {
-          reject(linkToEntity('id', data.table.nodes[0].__id));
+          reject(linkToEntity('id', data.table.nodes[0].__id, errMessage));
         } else {
           resolve();
         }
