8cfed7de2023e41a122496f18fe37818fa094aad
Nikita Kostin <n.kostin11@mail.ru>
2021-04-05 18:02:45 +0300

    Add includeDeletedPeople flag to PersonUniqueFieldValidator

--- js/packages/core/src/validator/PersonUniqueFieldValidator.tsx
+++ js/packages/core/src/validator/PersonUniqueFieldValidator.tsx
@@ -4,9 +4,10 @@ export async function PersonUniqueFieldValidator<T = string>(fieldName: string,
                                                              fieldValue: T,
                                                              errMessage: string,
                                                              excludePersonId: string = null,
-                                                             linkToEntity: (idFieldName: string, entityId: string, errMessage: string) => void): Promise<any> {
+                                                             linkToEntity: (idFieldName: string, entityId: string, errMessage: string) => void,
+                                                             includeDeletedPeople: boolean = true): Promise<any> {
   return new Promise((resolve, reject): any => {
-    DBUniqueValidator("allPeople", {[fieldName]: fieldValue, 'deleted': false}, 'id', excludePersonId)
+    DBUniqueValidator("allPeople", {[fieldName]: fieldValue, 'deleted': includeDeletedPeople}, 'id', excludePersonId)
       .then(data => {
         if (data.table.totalCount > 0) {
           reject(linkToEntity('id', data.table.nodes[0].__id, errMessage));
