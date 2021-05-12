0c562843a1354a655e185f1e674c8315e7be95d4
el1191 <el1191@woyd.ru>
2021-05-12 19:04:13 +0300

    SUDIR #SUI-37 Add custom message if user not found

--- java/sui-security/server/src/main/java/ru/sui/suisecurity/server/controller/SudirController.kt
+++ java/sui-security/server/src/main/java/ru/sui/suisecurity/server/controller/SudirController.kt
@@ -4,7 +4,9 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.web.bind.annotation.*
+import ru.sui.suisecurity.base.security.CustomUserDetailsService
 import ru.sui.suisecurity.base.utils.sudirTmpToken
 import ru.sui.suisecurity.server.model.LoginRequest
 import ru.sui.suisecurity.server.service.SudirService
@@ -14,7 +16,8 @@ import ru.sui.suisecurity.server.service.SudirService
 @ConditionalOnExpression("\${sudir.enabled:false}")
 class SudirController(
   val sudirService: SudirService,
-  val suiAuthController: SuiAuthController
+  val suiAuthController: SuiAuthController,
+  val customUserDetailsService: CustomUserDetailsService
 ) {
 
   @GetMapping("/callback")
@@ -28,6 +31,12 @@ class SudirController(
   fun sudirSignin(@RequestParam code: String): ResponseEntity<*> {
     val teResponse = sudirService.te(code)
     val meResponse = sudirService.me(teResponse.accessToken)
-    return suiAuthController.signin(LoginRequest(meResponse.email, sudirTmpToken))
+    val email = meResponse.email
+    try {
+      customUserDetailsService.loadUserByUsername(email)
+    } catch (e: UsernameNotFoundException) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Пользователь с email $email не найден")
+    }
+    return suiAuthController.signin(LoginRequest(email, sudirTmpToken))
   }
 }
