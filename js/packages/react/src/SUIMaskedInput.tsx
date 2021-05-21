78aab38810d250fc88117be5124087ea7036b231
HashAlexey <tiapkinalexey@gmail.com>
2021-05-21 15:35:20 +0300

    [SUIMaskedInput] Fix paste problem

--- js/packages/react/src/SUIMaskedInput.tsx
+++ js/packages/react/src/SUIMaskedInput.tsx
@@ -1,37 +1,55 @@
 import MaskedInput from 'antd-mask-input';
-import { MaskedInputProps } from 'antd-mask-input/build/main/lib/MaskedInput';
-import autobind from 'autobind-decorator';
+import {MaskedInputProps} from 'antd-mask-input/build/main/lib/MaskedInput';
 import React from 'react';
+import {v4 as uuidv4} from 'uuid';
 
-export type MyMaskedInputProps = Omit<MaskedInputProps, 'onChange'> & {
-  onChange?(value: string): void
+const defaultPasteFormatter = (it: string): string => it.replace(/[^0-9a-zа-яё]/gi, '');
+
+export interface ISUIMaskedInputProps extends Omit<MaskedInputProps, "id" | "onChange"> {
+  onChange?(value: string): void;
+  pasteFormatter?(value: string): string;
 }
 
-export class SUIMaskedInput extends React.Component<MyMaskedInputProps> {
-  private readonly inputRef: React.RefObject<MaskedInput> = React.createRef<MaskedInput>();
+export function SUIMaskedInput(props: ISUIMaskedInputProps): JSX.Element {
+  const {pasteFormatter, ...inputProps} = props;
 
-  public componentDidMount(): void {
-    this.inputRef.current.state.mask.setValue(this.props.value);
-  }
+  const id = React.useMemo(() => `SUIMaskedInput-${uuidv4()}`, []);
+  const inputRef = React.useRef<MaskedInput>();
 
-  public componentDidUpdate(): void {
-    this.inputRef.current.state.mask.setValue(this.props.value);
-  }
+  const onChange = (): void => {
+    if (inputProps.onChange) {
+      inputProps.onChange(inputRef.current.state.mask.getRawValue().replace(/_/g, ''))
+    }
+  };
 
-  public render(): React.ReactNode {
-    return (
-      <MaskedInput
-        {...this.props}
-        mask={this.props.mask}
-        ref={this.inputRef}
-        onChange={this.onChange}
-      />
-    )
-  }
+  // Old code ???
+  React.useEffect((): void => inputRef.current.state.mask.setValue(inputProps.value));
 
-  @autobind
-  private onChange(): void {
-    // this.props.onChange(this.inputRef.current.mask.getRawValue().replace(/[^\d]/g, ''));
-    this.props.onChange(this.inputRef.current.state.mask.getRawValue().replace(/_/g, ''));
-  }
+  // Paste magic effect
+  React.useEffect((): void => {
+    const input = document.getElementById(id) as HTMLInputElement;
+    input.onpaste = (e): void => {
+      const pastedValue = e.clipboardData.getData("Text");
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      e.clipboardData.getData = (format: string): string => {
+        if (format === "Text") {
+          return (pasteFormatter || defaultPasteFormatter)(pastedValue);
+        } else {
+          throw new Error(`Unexpected format: ${format}`);
+        }
+      }
+    };
+  });
+
+  return (
+    <MaskedInput
+      {...inputProps}
+      id={id}
+      mask={inputProps.mask}
+      ref={inputRef}
+      onChange={onChange}
+    />
+  );
 }
