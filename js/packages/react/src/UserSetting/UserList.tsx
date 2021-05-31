70cc73d7614878586eea04cc22fdced8a625b108
Nikita Kostin <n.kostin11@mail.ru>
2021-05-31 11:03:47 +0300

    Add nameValidator and userNameValidator to UserList #SOCMON-975

--- js/packages/react/src/UserSetting/UserList.tsx
+++ js/packages/react/src/UserSetting/UserList.tsx
@@ -30,6 +30,8 @@ interface IUserListProps<T> {
   handleDelete(ids: string[]): Promise<void>;
 
   isDeleted?(row: IObjectWithIndex): boolean;
+  nameValidator?(_: any, value: string, callback: any): void
+  userNameValidator?(_: any, value: string, callback: any): void
 }
 
 // eslint-disable-next-line @typescript-eslint/ban-types
@@ -52,6 +54,7 @@ export class UserList<T extends {}> extends React.Component<IUserListProps<T>> {
                   fieldName: "name",
                   inputNode: <Input placeholder="Введите ФИО"/>,
                   required: true,
+                  rules: [this.props.nameValidator ? {validator:  this.props.nameValidator} : {}],
                   title: "ФИО"
                 }
               }
@@ -73,7 +76,7 @@ export class UserList<T extends {}> extends React.Component<IUserListProps<T>> {
                   fieldName: "username",
                   inputNode: <Input placeholder="Введите имя пользователя"/>,
                   required: true,
-                  rules: [{min: MIN_USERNAME_LENGTH, message: MIN_USERNAME_LENGTH_MESSAGE}],
+                  rules: [this.props.userNameValidator ? {validator:  this.props.userNameValidator} : {min: MIN_USERNAME_LENGTH, message: MIN_USERNAME_LENGTH_MESSAGE}],
                   title: "Имя пользователя"
                 }
               }
