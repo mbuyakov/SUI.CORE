15a39c5d6aa41b0a8fd4d184623be73ad2145f5c
HashAlexey <tiapkinalexey@gmail.com>
2021-06-04 17:47:45 +0300

    Move trash to trash

--- js/packages/react/src/index.ts
+++ js/packages/react/src/index.ts
@@ -43,7 +43,6 @@ export * from "./Socket";
 export * from "./SparkLine";
 export * from "./soctech";
 export * from "./styles";
-export * from "./SUIInputNumber";
 export * from "./SUIMaskedInput";
 export * from "./SUIPhoneInput";
 export * from "./SUIReactComponent";
