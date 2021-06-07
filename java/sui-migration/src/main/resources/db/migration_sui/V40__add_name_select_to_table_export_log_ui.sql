ed86db1923c355b12c9fd38498ddea2389fedb6b
Nikita Kostin <n.kostin11@mail.ru>
2021-06-07 14:41:46 +0300

    Add table name catalog (log.table_name_ui) for table_name in log.table_export_log_ui #SDU-1004

---
+++ java/sui-migration/src/main/resources/db/migration_sui/V40__add_name_select_to_table_export_log_ui.sql
@@ -0,0 +1,7 @@
+CREATE VIEW log.table_name_ui AS
+SELECT DISTINCT COALESCE(name.name, table_info.table_name) AS id
+FROM log.table_export_log
+         INNER JOIN sui_meta.table_info ON table_export_log.table_info_id = table_info.id
+         LEFT JOIN sui_meta.name ON table_info.name_id = name.id;
+
+COMMENT ON COLUMN log.table_export_log_ui.table_name IS '@references log.table_name_ui(id)';
