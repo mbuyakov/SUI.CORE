5c6643b08a94aafb1e0b94b7865a58eb15c669c2
el1191 <el1191@woyd.ru>
2021-09-02 13:37:21 +0300

    Export UploadFile

--- js/packages/react/src/antdMissedExport.ts
+++ js/packages/react/src/antdMissedExport.ts
@@ -12,6 +12,7 @@ export type {FormListProps} from "antd/lib/form/FormList";
 export type {NamePath} from 'antd/lib/form/interface';
 export type {ButtonHTMLType} from "antd/lib/button/button";
 export type {ColumnType} from "antd/lib/table";
+export type {UploadFile} from "antd/lib/upload/interface";
 export {getThemeVariables} from 'antd/dist/theme';
 export {default as datePickerLocaleRu} from "antd/lib/date-picker/locale/ru_RU";
 export {default as localeRu} from "antd/lib/locale/ru_RU";
