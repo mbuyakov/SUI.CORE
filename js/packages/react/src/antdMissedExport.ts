5cafdcf748a05d7f775d1f04772f09d09493ae2a
el1191 <el1191@woyd.ru>
2021-09-02 13:27:55 +0300

    Export ColumnType

--- js/packages/react/src/antdMissedExport.ts
+++ js/packages/react/src/antdMissedExport.ts
@@ -11,7 +11,7 @@ export type {MenuItemProps} from "antd/lib/menu/MenuItem";
 export type {FormListProps} from "antd/lib/form/FormList";
 export type {NamePath} from 'antd/lib/form/interface';
 export type {ButtonHTMLType} from "antd/lib/button/button";
-
+export type {ColumnType} from "antd/lib/table";
 export {getThemeVariables} from 'antd/dist/theme';
 export {default as datePickerLocaleRu} from "antd/lib/date-picker/locale/ru_RU";
 export {default as localeRu} from "antd/lib/locale/ru_RU";
