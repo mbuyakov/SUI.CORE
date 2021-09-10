e3422f7236e01f326a20fa656841d5bc469f33a8
HashAlexey <tiapkinalexey@gmail.com>
2021-09-10 13:19:03 +0300

    Rewrite dateDisabler (current -> target)

--- js/packages/core/src/validator/utils.ts
+++ js/packages/core/src/validator/utils.ts
@@ -8,7 +8,7 @@ import moment, {Moment} from "moment";
  * @see lessDateDisabler
  * @see lessOrEqualDateDisabler
  */
-export function dateDisabler(bound: string | Moment, strategy: "greater" | "greaterOrEqual" | "less" | "lessOrEqual"): (current: Moment) => boolean {
+export function dateDisabler(bound: string | Moment, strategy: "greater" | "greaterOrEqual" | "less" | "lessOrEqual"): (target: Moment) => boolean {
   const boundMoment = typeof (bound) === "string" ? moment(bound) : bound;
 
   switch (strategy) {
@@ -28,27 +28,27 @@ export function dateDisabler(bound: string | Moment, strategy: "greater" | "grea
 /**
  * Disable all dates that are greater than the bound
  */
-export function greaterDateDisabler(bound: Moment): (current: Moment) => boolean {
-  return (current): boolean => current.isAfter(bound, "day");
+export function greaterDateDisabler(bound: Moment): (target: Moment) => boolean {
+  return (target): boolean => target.isAfter(bound, "day");
 }
 
 /**
  * Disable all dates that are greater or equal than the bound
  */
-export function greaterOrEqualDateDisabler(bound: Moment): (current: Moment) => boolean {
-  return (current): boolean => current.isSameOrAfter(bound, "day");
+export function greaterOrEqualDateDisabler(bound: Moment): (target: Moment) => boolean {
+  return (target): boolean => target.isSameOrAfter(bound, "day");
 }
 
 /**
  * Disable all dates that are less than the bound
  */
-export function lessDateDisabler(bound: Moment): (current: Moment) => boolean {
-  return (current): boolean => current.isBefore(bound, "day");
+export function lessDateDisabler(bound: Moment): (target: Moment) => boolean {
+  return (target): boolean => target.isBefore(bound, "day");
 }
 
 /**
  * Disable all dates that are less or equal than the bound
  */
-export function lessOrEqualDateDisabler(bound: Moment): (current: Moment) => boolean {
-  return (current): boolean => current.isSameOrBefore(bound, "day");
+export function lessOrEqualDateDisabler(bound: Moment): (target: Moment) => boolean {
+  return (target): boolean => target.isSameOrBefore(bound, "day");
 }
