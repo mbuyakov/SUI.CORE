859304604124acb025fc1ffcea0081ccc47dc4e7
HashAlexey <tiapkinalexey@gmail.com>
2021-09-17 19:29:47 +0300

    UTC for log.populate_audit_columns

---
+++ java/sui-migration/src/main/resources/db/migration_sui/V40__utc_for_populate_audit_columns.sql
@@ -0,0 +1,51 @@
+CREATE OR REPLACE FUNCTION log.populate_audit_columns()
+    RETURNS TRIGGER
+    SECURITY DEFINER
+    LANGUAGE plpgsql
+AS
+$$
+    DECLARE
+        table_info sui_meta.table_info;
+        columns TEXT[];
+        field_patch JSONB = '{}'::JSONB;
+        update_delta JSONB;
+    BEGIN
+        table_info := sui_meta.get_table_info_by_oid(TG_RELID);
+
+        SELECT array_agg(column_name::TEXT)
+        FROM information_schema.columns
+        WHERE columns.table_schema = table_info.schema_name
+          AND columns.table_name = table_info.table_name
+        INTO columns;
+
+        -- Set change_user_id for INSERT AND UPDATE
+        IF 'change_user_id' = ANY(columns) THEN
+            field_patch := field_patch || jsonb_build_object('change_user_id', sui_security.get_current_user_id());
+        END IF;
+
+        IF TG_OP = 'INSERT' THEN
+            IF 'create_user_id' = ANY(columns) THEN
+                field_patch := field_patch || jsonb_build_object('create_user_id', sui_security.get_current_user_id());
+            END IF;
+        ELSE -- TG_OP = 'UPDATE'
+            IF (ARRAY['modified', 'delete_date'] && columns) THEN
+                update_delta := sui_utils.jsonb_difference(to_jsonb(OLD), to_jsonb(NEW), 'old', 'new');
+
+                -- IF modified
+                IF (update_delta->>'old' IS NOT NULL OR update_delta->>'new' IS NOT NULL) THEN
+                    IF 'modified' = ANY(columns) THEN
+                        field_patch := field_patch || jsonb_build_object('modified', now() AT TIME ZONE 'UTC');
+                    END IF;
+
+                    IF 'deleted' = ANY(columns) AND 'delete_date' = ANY(columns) THEN
+                        IF ((update_delta->'old'->>'deleted')::BOOLEAN IS FALSE AND (update_delta->'new'->>'deleted')::BOOLEAN IS TRUE ) THEN
+                            field_patch := field_patch || jsonb_build_object('delete_date', now() AT TIME ZONE 'UTC');
+                        END IF;
+                    END IF;
+                END IF;
+            END IF;
+        END IF;
+
+        RETURN json_populate_record(NEW, field_patch::JSON);
+    END;
+$$;
