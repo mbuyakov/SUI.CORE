7f0b314ee96538ad3757fc0bcf167e0f09a51cec
HashAlexey <tiapkinalexey@gmail.com>
2021-09-20 13:47:08 +0300

    [Backend/WebMvcConfig] Add configureAsyncSupport

--- java/sui-backend/src/main/java/ru/sui/suibackend/config/WebMvcConfig.java
+++ java/sui-backend/src/main/java/ru/sui/suibackend/config/WebMvcConfig.java
@@ -2,22 +2,29 @@ package ru.sui.suibackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.Executors;
 
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
-    private final long MAX_AGE_SECS = 3600;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
-                .maxAge(MAX_AGE_SECS);
+                .maxAge(3600);
+    }
+
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+        configurer.setTaskExecutor(new ConcurrentTaskExecutor(Executors.newFixedThreadPool(5)));
     }
 
     @Bean
