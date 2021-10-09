9fd3a31d8732e05a998c770347ca4d118c017375
el1191 <el1191@woyd.ru>
2021-10-10 02:55:56 +0300

    Fix sui_flyway_schema_history kludge for empty DB

--- java/sui-migration/src/main/java/ru/sui/migration/FlywayMigrationApplier.kt
+++ java/sui-migration/src/main/java/ru/sui/migration/FlywayMigrationApplier.kt
@@ -1,10 +1,13 @@
 package ru.sui.migration
 
+import mu.KotlinLogging
 import org.flywaydb.core.Flyway
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
+import java.lang.Exception
 import javax.annotation.PostConstruct
 
+private val log = KotlinLogging.logger { }
 
 @Service
 class FlywayMigrationApplier(private val jdbcTemplate: JdbcTemplate) {
@@ -12,11 +15,17 @@ class FlywayMigrationApplier(private val jdbcTemplate: JdbcTemplate) {
     @Suppress("SqlDialectInspection", "SqlNoDataSourceInspection")
     @PostConstruct
     fun postConstruct() {
-        jdbcTemplate.update("""
+      try {
+        jdbcTemplate.update(
+          """
             UPDATE public.sui_flyway_schema_history
             SET checksum = -454516158
             WHERE version = '8' AND checksum != -454516158;
-        """)
+        """
+        )
+      } catch (e: Exception) {
+        log.error(e) { "Can't update migration checksum" }
+      }
 
         val flyway = Flyway
                 .configure()
