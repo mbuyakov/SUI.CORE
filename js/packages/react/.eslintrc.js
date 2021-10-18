cc9053bd1cfa79aefc18f095d6dc6676fd3c7723
el1191 <el1191@woyd.ru>
2021-10-18 23:28:11 +0300

    Update linter

--- js/packages/react/.eslintrc.js
+++ js/packages/react/.eslintrc.js
@@ -1,14 +1,5 @@
-const origin = require("@sui/linter/.eslintrc.js");
+const origin = require("@sui/linter/.eslintrc-lib.js");
 
 module.exports = {
   ...origin,
-  rules: {
-    ...origin.rules,
-    "no-use-before-define": [0],
-    "camelcase": [0],
-    "no-console": [0],
-    "react/jsx-indent-props": [1],
-    "@typescript-eslint/no-empty-interface": [1],
-    "react/static-property-placement": [1]
-  }
 }
--- js/packages/sentry/.eslintrc.js
+++ js/packages/sentry/.eslintrc.js
@@ -1,11 +1,5 @@
-const origin = require("@sui/linter/.eslintrc.js");
+const origin = require("@sui/linter/.eslintrc-lib.js");
 
 module.exports = {
   ...origin,
-  rules: {
-    ...origin.rules,
-    "no-use-before-define": [0],
-    "camelcase": [0],
-    "no-console": [0]
-  }
 }
