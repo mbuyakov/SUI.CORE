afaa44144b8c482d4189555ced201dfcf3dfbb0d
HashAlexey <tiapkinalexey@gmail.com>
2021-12-29 12:43:52 +0300

    Remove multiple token check error notifications

--- js/packages/core/src/ioc/service/UserService.ts
+++ js/packages/core/src/ioc/service/UserService.ts
@@ -76,7 +76,10 @@ export class UserService<META = Record<string, never>> {
           }
         } catch (reason) {
           this.log.error(reason, 'Token check error');
-          notification.warn({message: 'Ошибка при проверке токена'});
+          notification.warn({
+            message: 'Ошибка при проверке токена',
+            key: 'TOKEN_CHECK_ERROR_NOTIFICATION'
+          });
           await sleep(1000);
         }
       } else {
