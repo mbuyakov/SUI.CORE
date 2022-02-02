b44303edb0817a5e5e732a4da76893b43a4b79af
ites777 <ites777@gmail.com>
2022-02-02 12:35:16 +0300

    fix EmptyMessageComponent

--- js/packages/react/src/BaseTable/extends/EmptyMessageComponent.tsx
+++ js/packages/react/src/BaseTable/extends/EmptyMessageComponent.tsx
@@ -4,6 +4,6 @@ import * as React from 'react';
 export const EmptyMessageComponent = (): JSX.Element => (
   <Empty
     description="Нет выбранных колонок"
-    style={{width: '100%', margin: 10}}
+    style={{maxWidth: '100%', margin: 10}}
   />
 );
