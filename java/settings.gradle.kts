b4cb2b46e39cf88bb14f6d7c7f87202dc0f7dd8e
el1191 <el1191@woyd.ru>
2022-02-28 13:21:10 +0300

    Remove utils modules directories

--- java/settings.gradle.kts
+++ java/settings.gradle.kts
@@ -8,10 +8,10 @@ include(":sui-migration")
 include(":sui-security:base")
 include(":sui-security:server")
 include(":sui-user-transaction")
-include(":sui-utils:sui-utils-hbase")
-include(":sui-utils:sui-utils-hdfs")
-include(":sui-utils:sui-utils-kotlin")
-include(":sui-utils:sui-utils-parquet")
+include(":sui-utils:hbase")
+include(":sui-utils:hdfs")
+include(":sui-utils:kotlin")
+include(":sui-utils:parquet")
 
 rootProject.name = "sui"
 
@@ -20,3 +20,7 @@ project(":sui-audit:log-mover").name = "sui-audit-log-mover"
 project(":sui-audit:manager").name = "sui-audit-manager"
 project(":sui-security:base").name = "sui-security-base"
 project(":sui-security:server").name = "sui-security-server"
+project(":sui-utils:hbase").name = "sui-utils-hbase"
+project(":sui-utils:hdfs").name = "sui-utils-hdfs"
+project(":sui-utils:kotlin").name = "sui-utils-kotlin"
+project(":sui-utils:parquet").name = "sui-utils-parquet"
