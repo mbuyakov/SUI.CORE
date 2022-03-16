6008c1a20683a15c77ec8b4cae0e83fe44d5fc34
HashAlexey <tiapkinalexey@gmail.com>
2022-03-16 22:03:15 +0300

    Add passwordValidator

--- js/packages/react/src/UserSetting/const.tsx
+++ js/packages/react/src/UserSetting/const.tsx
@@ -1,3 +1,5 @@
+import {Nullable} from "@sui/core/src";
+
 export const MIN_NAME_LENGTH = 2;
 export const MIN_NAME_LENGTH_MESSAGE = "Минимальная длина - 2 символа";
 
@@ -6,9 +8,48 @@ export const MIN_USERNAME_LENGTH_MESSAGE = "Минимальная длина - 
 
 export const MIN_PASSWORD_LENGTH = 8;
 export const MIN_PASSWORD_LENGTH_MESSAGE = 'Минимальная длина - 8 символов';
+
 export const MAX_PASSWORD_LENGTH = 50;
 export const MAX_PASSWORD_LENGTH_MESSAGE = 'Максимальная длина - 50 символов';
 
 // https://stackoverflow.com/a/21456918
 export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!-*])[A-Za-z\d!\-*]+$/;
 export const PASSWORD_REGEX_MESSAGE = "Пароль должен состоять из заглавных и прописных латинских букв A-z, цифр 0-9 и специальных символов !-*";
+
+interface IPasswordValidatorParams {
+  password: Nullable<string>;
+  username: Nullable<string>;
+  email: Nullable<string>;
+  callback: (error: string | string[] | void) => void
+}
+
+export function passwordValidator({password, username, email, callback}: IPasswordValidatorParams): void {
+  const notNullPassword = password || "";
+
+  if (notNullPassword.length < MIN_PASSWORD_LENGTH) {
+    callback(MIN_PASSWORD_LENGTH_MESSAGE);
+    return;
+  }
+
+  if (notNullPassword.length > MAX_PASSWORD_LENGTH) {
+    callback(MAX_PASSWORD_LENGTH_MESSAGE);
+    return;
+  }
+
+  if (!PASSWORD_REGEX.test(notNullPassword)) {
+    callback(PASSWORD_REGEX_MESSAGE);
+    return;
+  }
+
+  if (notNullPassword.trim() === username?.trim()) {
+    callback("Пароль не должен совпадать с именем пользователя");
+    return;
+  }
+
+  if (notNullPassword.trim() === email?.trim()) {
+    callback("Пароль не должен совпадать с адресом электронной почты");
+    return;
+  }
+
+  callback();
+}
