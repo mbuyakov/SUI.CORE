85ecd5df4295ca38a30a4c574dd0c12ff31742dd
el1191 <el1191@woyd.ru>
2022-06-20 15:42:36 +0300

    Add common contexts to draw()

--- js/packages/react/src/utils/draw.ts
+++ js/packages/react/src/utils/draw.ts
@@ -1,15 +1,21 @@
 import * as React from "react";
 import * as ReactDOM from 'react-dom';
+import {Container} from "typescript-ioc";
+import {ThemeProvider} from "@material-ui/core/styles";
+import {ConfigProvider} from "antd";
+import {SuiThemeContext, ThemeService} from "@/themes";
+import {localeRu} from "@/antdMissedExport";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function draw(element: React.ReactElement, containerRef?: React.RefObject<any>): void {
+export function draw(element: React.ReactElement, containerRef?: React.RefObject<never>): void {
+  const theme = Container.get(ThemeService).getCurrentTheme();
   let elementContainer;
 
   elementContainer = containerRef && containerRef.current
     ? ReactDOM.findDOMNode(containerRef.current)
     : document.createElement('div');
 
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  ReactDOM.render(element, elementContainer);
+  ReactDOM.render(
+    React.createElement(SuiThemeContext.Provider, {value: theme},
+      React.createElement(ConfigProvider, {locale: localeRu},
+        React.createElement(ThemeProvider, {theme: theme.muiTheme, children: element}))), elementContainer);
 }
