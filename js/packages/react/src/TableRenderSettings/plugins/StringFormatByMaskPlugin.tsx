c1dbc2f24b6ac5e54f8ed17d7ae24567223bf7d4
Nikita Kostin <n.kostin11@mail.ru>
2022-09-09 09:36:44 +0300

    Add format by mask plugin

---
+++ js/packages/react/src/TableRenderSettings/plugins/StringFormatByMaskPlugin.tsx
@@ -0,0 +1,42 @@
+import {ITableRenderParams, TableRenderParamsPlugin, TableRenderSettingsPluginManager, TableRenderSettingsPopover} from "@/TableRenderSettings";
+import {IBaseTableColLayout} from "@/BaseTable";
+import {IColumnInfoToBaseTableColProps} from "@/utils";
+import * as React from "react";
+import {Input} from "antd";
+import {formatByMaskFn} from "@/tmp";
+
+export interface IStringFormatByMaskPluginTRP {
+  mask?: string;
+}
+
+export class StringFormatByMaskPlugin extends TableRenderParamsPlugin<IStringFormatByMaskPluginTRP> {
+
+  public constructor() {
+    super('stringByMaskFormatter', 'Форматирование строки по маске', true);
+  }
+
+  baseTableColGenerator(result: IBaseTableColLayout, renderColumnInfo: | null, props: IColumnInfoToBaseTableColProps, trp: ITableRenderParams<IStringFormatByMaskPluginTRP>): Promise<void> {
+    const {mask} = trp;
+    result.render = (value: string): React.ReactNode => {
+      if (!value && !mask) {
+        return value;
+      }
+
+      return formatByMaskFn(mask)(value);
+    };
+  }
+
+  public getSettingsPopoverContent(trsp: TableRenderSettingsPopover<IStringFormatByMaskPluginTRP>): React.ReactNode {
+    return (
+      <>
+        <span>Маска:</span>
+        <Input
+          value={trsp.state.tableRenderParams.mask || undefined}
+          onChange={(e): Promise<void> => trsp.updateField('mask')(e.target.value)}
+        />
+      </>
+    )
+  }
+}
+
+TableRenderSettingsPluginManager.register(new StringFormatByMaskPlugin());
--- js/packages/react/src/TableRenderSettings/plugins/index.ts
+++ js/packages/react/src/TableRenderSettings/plugins/index.ts
@@ -3,6 +3,7 @@ export * from "./LinkPlugin";
 export * from "./MultipleLinkPlugin";
 export * from "./RawPlugin";
 export * from "./RoundPlugin";
+export * from "./StringFormatByMaskPlugin";
 export * from "./SparklinePlugin";
 export * from "./TableRenderParamsPlugin";
 export * from "./UnknownPlugin";
