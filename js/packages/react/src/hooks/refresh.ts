f3741ff93dd17e4490b957e6e4d6bde87fdcd741
el1191 <el1191@woyd.ru>
2022-11-18 12:43:19 +0300

    Fix

--- js/packages/react/src/hooks/refresh.ts
+++ js/packages/react/src/hooks/refresh.ts
@@ -1,7 +1,7 @@
 import {v4 as uuidv4} from 'uuid';
 import {useState} from "react";
 
-export function useRefresh(): [value: string, refresh: () => void] {
+export function useRefresh(): [string, () => void] {
   const [state, setState] = useState(uuidv4());
   const refresh = () => setState(uuidv4())
   return [state, refresh];
