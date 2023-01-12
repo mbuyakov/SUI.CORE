dbadc23f39663f66f7d3721d89b9a4b0888a003f
HashAlexey <tiapkinalexey@gmail.com>
2023-01-12 14:42:47 +0300

    Add _sui_token cookie

--- java/sui-security/server/src/main/java/ru/sui/suisecurity/server/model/JwtAuthenticationResponse.kt
+++ java/sui-security/server/src/main/java/ru/sui/suisecurity/server/model/JwtAuthenticationResponse.kt
@@ -1,12 +1,21 @@
 package ru.sui.suisecurity.server.model
 
+import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
 import ru.sui.suisecurity.base.model.LoginResult
 import ru.sui.suisecurity.base.security.UserPrincipal
-
+import java.net.HttpCookie
 
 fun LoginResult.toResponseEntity(): ResponseEntity<JwtAuthenticationResponse> {
-    return ResponseEntity.ok(JwtAuthenticationResponse(this.jwt, this.principal))
+    val tokenCookie = HttpCookie("_sui_token", this.jwt)
+
+    tokenCookie.path = "/"
+    tokenCookie.secure = true
+    tokenCookie.isHttpOnly = true
+
+    return ResponseEntity.ok()
+        .header(HttpHeaders.SET_COOKIE, tokenCookie.toString())
+        .body(JwtAuthenticationResponse(this.jwt, this.principal))
 }
 
 class JwtAuthenticationResponse(
