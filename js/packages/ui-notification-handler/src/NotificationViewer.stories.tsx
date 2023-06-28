71304d3ac905f2406e95cddf1b7cda12c416e4a8
el1191 <el1191@woyd.ru>
2023-06-28 04:28:23 +0300

    chore(ui-notification-handler): add new example

--- js/packages/ui-notification-handler/src/NotificationViewer.stories.tsx
+++ js/packages/ui-notification-handler/src/NotificationViewer.stories.tsx
@@ -15,16 +15,14 @@ export default meta;
 type Story = ComponentStory<typeof _NotificationViewer>;
 
 const Template: Story = (args) => (
-  <>
-    <Stack spacing={2} alignItems="start">
-      <Button variant="outlined" onClick={() => Container.get(NotificationDispatcher).success("title", "message")}>Success</Button>
-      <Button variant="outlined" onClick={() => Container.get(NotificationDispatcher).info("title", "message")}>Info</Button>
-      <Button variant="outlined" onClick={() => Container.get(NotificationDispatcher).warning("title", "message")}>Warn</Button>
-      <Button variant="outlined" onClick={() => Container.get(NotificationDispatcher).error("title", "message")}>Error</Button>
-      <Button variant="outlined" onClick={() => Container.get(NotificationDispatcher).handleError(new Error("Ашипка"))}>Throw</Button>
-      <Button variant="outlined" disabled={true}>Disabled button</Button>
-    </Stack>
-  </>
+  <Stack spacing={2} alignItems="start">
+    <Button variant="outlined" onClick={() => Container.get(NotificationDispatcher).success("title", "message")}>Success</Button>
+    <Button variant="outlined" onClick={() => Container.get(NotificationDispatcher).info("title", "message")}>Info</Button>
+    <Button variant="outlined" onClick={() => Container.get(NotificationDispatcher).info("title", "message", {key: "static"})}>Info with static key</Button>
+    <Button variant="outlined" onClick={() => Container.get(NotificationDispatcher).warning("title", "message")}>Warn</Button>
+    <Button variant="outlined" onClick={() => Container.get(NotificationDispatcher).error("title", "message")}>Error</Button>
+    <Button variant="outlined" onClick={() => Container.get(NotificationDispatcher).handleError(new Error("Ашипка"))}>Throw</Button>
+  </Stack>
 );
 
 export const NotificationViewer = Template.bind({});
