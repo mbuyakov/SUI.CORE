888fa10e63ba0286fcd4ef0b0f21d1a1c9f65d4b
el1191 <el1191@woyd.ru>
2023-06-29 23:22:53 +0300

    chore(deps-ioc): add test for class with impl

--- js/packages/deps-ioc/src/SingletonAndOnlyIoc.ts.spec.ts
+++ js/packages/deps-ioc/src/SingletonAndOnlyIoc.ts.spec.ts
@@ -5,6 +5,12 @@ class SmthClass {
 
 }
 
+class SmthClassImpl extends SmthClass {
+
+}
+
+
+
 const _SmthClass = SingletonAndOnlyIoc(SmthClass);
 
 describe("SingletonAndOlyIoc", () => {
@@ -17,5 +23,18 @@ describe("SingletonAndOlyIoc", () => {
     const smthClass2 = Container.get(_SmthClass);
     expect(smthClass1).toBeDefined();
     expect(smthClass1).toBe(smthClass2);
+    expect(smthClass1 instanceof SmthClass).toBe(true);
+    expect(smthClass1 instanceof SmthClassImpl).toBe(false);
+
+  });
+
+  test("Singleton with impl check", () => {
+    Container.bind(_SmthClass).to(SmthClassImpl);
+    const smthClass1 = Container.get(_SmthClass);
+    const smthClass2 = Container.get(_SmthClass);
+    expect(smthClass1).toBeDefined();
+    expect(smthClass1).toBe(smthClass2);
+    expect(smthClass1 instanceof SmthClass).toBe(true);
+    expect(smthClass1 instanceof SmthClassImpl).toBe(true);
   });
 });
