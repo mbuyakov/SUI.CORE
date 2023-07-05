801f0e8eaabfdde9b851d81fc8fbb30d86d1060e
el1191 <el1191@woyd.ru>
2023-07-06 02:11:06 +0300

    feat(nx-plugin): auto update paths kludge for old packages

--- js/packages/nx-plugin/src/generators/util.ts
+++ js/packages/nx-plugin/src/generators/util.ts
@@ -1,4 +1,4 @@
-import {removeDependenciesFromPackageJson, Tree, updateProjectConfiguration} from "@nx/devkit";
+import {removeDependenciesFromPackageJson, Tree, updateJson, readJson, updateProjectConfiguration} from "@nx/devkit";
 import {addMinimalPublishScript} from "@nx/js/src/utils/minimal-publish-script";
 import {readProjectConfiguration} from "nx/src/generators/utils/project-configuration";
 
@@ -21,4 +21,17 @@ export const commonTweaks = (tree: Tree, type: string, name: string) => {
   tree.delete(".prettierignore");
   tree.delete(".prettierrc");
   removeDependenciesFromPackageJson(tree, [], ["prettier"]);
+
+  const paths = readJson(tree, "tsconfig.base.json").compilerOptions.paths;
+
+  updateJson(tree, "packages/ui-old-core/tsconfig.json", config => {
+    config.compilerOptions.paths = paths;
+    config.compilerOptions.paths["@/*"] = ["packages/ui-old-core/src/*"];
+    return config;
+  });
+  updateJson(tree, "packages/ui-old-react/tsconfig.json", config => {
+    config.compilerOptions.paths = paths;
+    config.compilerOptions.paths["@/*"] = ["packages/ui-old-react/src/*"];
+    return config;
+  });
 };
