33522f4ba2f0b2ea47f01f68d89d12105f792287
el1191 <el1191@woyd.ru>
2023-07-05 11:39:02 +0300

    feat(nx-plugin): update sui-migrator

--- js/packages/nx-plugin/src/generators/sui-migrator/generator.spec.ts
+++ js/packages/nx-plugin/src/generators/sui-migrator/generator.spec.ts
@@ -3,15 +3,21 @@ import {remapImports} from "./generator";
 describe("sui-migrator generator", () => {
   it("remapImports", () => {
     const prev = `
-    import {test1} from "@nemui/material";
-    import {test2} from "@mui/material";
-    import {test3} from "antd";
-    `;
-
-    expect(remapImports("@sui/deps-antd", prev)).toBe(`
-    import {test1} from "@nemui/material";
-    import {test2} from "@sui/deps-material";
-    import {test3} from "antd";
-    `);
+import {test1} from "@nemui/material";
+import {test2} from "@mui/material";
+import {test3} from "antd";
+import {test4} from "@mui/material/test";
+import {test5, test6} from "@mui/material/test";
+import test7 from "@mui/material/test";
+    `.trim();
+    const after = `
+import {test1} from "@nemui/material";
+import { test2 } from "@sui/deps-material";
+import {test3} from "antd";
+import { test4 } from "@sui/deps-material";
+import { test5, test6 } from "@sui/deps-material";
+import { test7 } from "@sui/deps-material";
+    `.trim();
+    expect(remapImports("deps-antd", prev).trim()).toBe(after);
   });
 });
--- js/packages/nx-plugin/src/generators/sui-migrator/generator.ts
+++ js/packages/nx-plugin/src/generators/sui-migrator/generator.ts
@@ -2,12 +2,13 @@ import {Tree} from "@nx/devkit";
 import {tsquery} from "@phenomnomnominal/tsquery";
 import {getProjects} from "nx/src/generators/utils/project-configuration";
 import {SuiMigratorGeneratorSchema} from "./schema";
+import {createPrinter, EmitHint, factory, ImportDeclaration, NewLineKind, StringLiteral} from "typescript";
 
-function visitAllFiles(
-  tree: Tree,
-  path: string,
-  callback: (filePath: string) => void
-) {
+const printer = createPrinter({
+  newLine: NewLineKind.LineFeed,
+});
+
+function visitAllFiles(tree: Tree, path: string, callback: (filePath: string) => void) {
   tree.children(path).forEach((fileName) => {
     const filePath = `${path}/${fileName}`;
     console.log(`Process path ${filePath}`);
@@ -28,28 +29,52 @@ const mapModules = {
 };
 
 export function remapImports(projectName: string, content: string): string {
-  Object.keys(mapModules).forEach(oldName => {
-    const newName = mapModules[oldName];
-    if (newName == `@sui/${projectName}`) {
+
+  return tsquery.replace(content, "ImportDeclaration", (node: ImportDeclaration) => {
+    let newName: string;
+
+    for (const oldName of Object.keys(mapModules)) {
+      if (mapModules[oldName] != `@sui/${projectName}` // Don't replace yourself
+        && (node.moduleSpecifier as StringLiteral).text.startsWith(oldName) // If import contain out managed modules
+      ) {
+        newName = mapModules[oldName];
+      }
+    }
+
+    if (!newName) {
       return;
     }
-    content = tsquery.replace(content, `ImportDeclaration:has(StringLiteral[value="${oldName}"])`, node => node.getText().replace(oldName, newName));
-  });
 
-  return content;
+    // If used import default - probably it's from submodule, replace with named import
+    const namedImports = node.importClause.namedBindings || factory.createNamedImports([
+      factory.createImportSpecifier(node.importClause.isTypeOnly, undefined, node.importClause.name)
+    ]);
+
+    node = factory.updateImportDeclaration(
+      node,
+      node.modifiers,
+      factory.updateImportClause(
+        node.importClause,
+        node.importClause.isTypeOnly,
+        undefined,
+        namedImports
+      ),
+      factory.createStringLiteral(newName),
+      node.assertClause
+    );
+
+    return printer.printNode(EmitHint.Unspecified, node, node.getSourceFile());
+  });
 }
 
 export async function suiMigratorGenerator(tree: Tree, schema: SuiMigratorGeneratorSchema) {
   const projects = getProjects(tree);
-
   projects.forEach(project => {
     console.log(`Process project ${project.name}`);
     visitAllFiles(tree, project.sourceRoot, (filePath) => {
       const fileEntry = tree.read(filePath);
       const content = fileEntry.toString();
-
-      const newContent = remapImports(project.name, content);
-
+      let newContent = remapImports(project.name, content);
       if (newContent !== content) {
         tree.write(filePath, newContent);
       }
