609a151e0d2c87c66f92344dfaa6ec7540c0ca1f
el1191 <el1191@woyd.ru>
2023-07-06 02:41:40 +0300

    feat(util-types): add NotFunction

--- js/packages/util-types/src/index.ts
+++ js/packages/util-types/src/index.ts
@@ -2,6 +2,9 @@ export type Class<T> = new (...args: never[]) => T;
 
 export type Nullable<T> = T | null | undefined;
 
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type NotFunction<T> = T extends Function ? never : T;
+
 export interface IObjectWithIndex {
   [index: string]: any;
 }
