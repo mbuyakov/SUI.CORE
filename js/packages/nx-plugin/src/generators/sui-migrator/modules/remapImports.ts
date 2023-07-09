30d466d2b36f9b622468b0540d09fa6b9b468944
el1191 <el1191@woyd.ru>
2023-07-09 05:56:30 +0300

    chore(nx-plugin): simplify remapImports

--- js/packages/nx-plugin/src/generators/sui-migrator/modules/remapImports.ts
+++ js/packages/nx-plugin/src/generators/sui-migrator/modules/remapImports.ts
@@ -1,47 +1,47 @@
 import {factory, ImportDeclaration, StringLiteral} from "typescript";
-import {mapModules} from  "../../../utils/consts";
+import {mapModules} from "../../../utils/consts";
 import {logWithPrefix} from "../../../utils/logger";
 import {printNode, tsquery} from "../../../utils/typescript";
 
 export function remapImports(projectName: string, content: string): string {
 
-  return tsquery.replace(content, "ImportDeclaration", (node: ImportDeclaration) => {
+  return tsquery.replace(content, "ImportDeclaration:has(ImportClause)", (node: ImportDeclaration) => {
     const moduleName = (node.moduleSpecifier as StringLiteral).text;
-    let newName: string;
+    let newName = Object.entries(mapModules)
+      .find(([oldName]) => moduleName == oldName || moduleName.startsWith(oldName + "/"))
+      ?.[1];
 
-    for (const oldName of Object.keys(mapModules)) {
-      if (
-        mapModules[oldName] != `@sui/${projectName}` // Don't replace yourself
-        && (moduleName == oldName || moduleName.startsWith(oldName + "/")) // If import contain out managed modules
-        ) {
-        newName = mapModules[oldName];
-      }
-    }
-
-    if (!newName) {
+    if (
+      !newName
+      // Don't replace yourself
+      || newName === `@sui/${projectName}`
+    ) {
       return;
     }
 
     logWithPrefix("remapImports", `Replace ${moduleName} to ${newName}`);
 
-    // If used import default - probably it's from submodule, replace with named import
-    const namedImports = node.importClause.namedBindings || factory.createNamedImports([
-      factory.createImportSpecifier(node.importClause.isTypeOnly, undefined, node.importClause.name)
-    ]);
+    let namedImports = node.importClause.namedBindings;
 
-    node = factory.updateImportDeclaration(
-      node,
-      node.modifiers,
-      factory.updateImportClause(
-        node.importClause,
-        node.importClause.isTypeOnly,
-        undefined,
-        namedImports
-      ),
-      factory.createStringLiteral(newName),
-      node.assertClause
+    // If used default import - probably it's from submodule, replace with named import
+    if (!namedImports) {
+      namedImports = factory.createNamedImports([
+        factory.createImportSpecifier(node.importClause.isTypeOnly, undefined, node.importClause.name)
+      ]);
+    }
+    return printNode(
+      factory.updateImportDeclaration(
+        node,
+        node.modifiers,
+        factory.updateImportClause(
+          node.importClause,
+          node.importClause.isTypeOnly,
+          undefined,
+          namedImports
+        ),
+        factory.createStringLiteral(newName),
+        node.assertClause
+      )
     );
-
-    return printNode(node);
   });
 }
