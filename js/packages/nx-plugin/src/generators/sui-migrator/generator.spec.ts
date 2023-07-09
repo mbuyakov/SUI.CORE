144fe18552b32e9b61bae7c1a0614166280c3033
el1191 <el1191@woyd.ru>
2023-07-09 06:01:46 +0300

    chore(nx-plugin): drop unacrual test

--- js/packages/nx-plugin/src/generators/sui-migrator/generator.spec.ts
+++ js/packages/nx-plugin/src/generators/sui-migrator/generator.spec.ts
@@ -1,75 +1,4 @@
-import {remapIcons} from "./modules/remapIcons";
-import {remapImports} from "./modules/remapImports";
-import {dedupeImport} from "./modules/dedupeImport";
 import {remapOldPackages} from "./modules/remapOldPackages";
-
-const source = `
-import {test1} from "@nemui/material";
-import {test2} from "@mui/material";
-import {test3} from "antd";
-import {test4} from "@mui/material/test";
-import {test5, test6} from "@mui/material/test";
-import test7 from "@mui/material/test";
-import {test8} from "antd";
-import {test9} from "@mui/material-fake";
-import {test10} from "antd/lib";
-import MenuIcon from "@mui/icons-material/Menu";
-import {DeleteForever} from "@mui/icons-material";
-import {Container} from "typescript-ioc";
-import * as colors from "colors";
-import test11 from "colors";
-import {test12} from "colors";
-import "odometer/themes/odometer-theme-default.css";
-import {DataKey, Test} from "@sui/ui-old-react";
-import {dataKeysToDataTree} from "@sui/ui-old-core";
-
-const a = <MenuIcon attr="1">2</MenuIcon>;
-const b = <MenuIcon attr="1"/>;
-
-const c = <DeleteForever attr="1">2</DeleteForever>;
-const d = <DeleteForever attr="1"/>;
-
-const f = <DrawerVisibleButton
-            edge="start"
-            disableRotate={isMobile}
-            icon={isMobile && <MenuIcon/>}
-          />;
-const g = MenuIcon as any;
-    `.trim();
-
-const afterRemapImports = `
-import {test1} from "@nemui/material";
-import {test2} from "@sui/deps-material";
-import {test3} from "antd";
-import {test4} from "@sui/deps-material";
-import {test5, test6} from "@sui/deps-material";
-import {test7} from "@sui/deps-material";
-import {test8} from "antd";
-import {test9} from "@mui/material-fake";
-import {test10} from "antd/lib";
-import MenuIcon from "@mui/icons-material/Menu";
-import {DeleteForever} from "@mui/icons-material";
-import {Container} from "@sui/deps-ioc";
-import * as colors from "colors";
-import test11 from "colors";
-import {test12} from "colors";
-import "odometer/themes/odometer-theme-default.css";
-import {DataKey, Test} from "@sui/ui-old-react";
-import {dataKeysToDataTree} from "@sui/ui-old-core";
-
-const a = <MenuIcon attr="1">2</MenuIcon>;
-const b = <MenuIcon attr="1"/>;
-
-const c = <DeleteForever attr="1">2</DeleteForever>;
-const d = <DeleteForever attr="1"/>;
-
-const f = <DrawerVisibleButton
-            edge="start"
-            disableRotate={isMobile}
-            icon={isMobile && <MenuIcon/>}
-          />;
-const g = MenuIcon as any;
-    `.trim();
 
 const afterRemapIcons = `
 import {MuiIcons} from "@sui/deps-material";
@@ -138,47 +67,8 @@ const f = <DrawerVisibleButton
 const g = MuiIcons.Menu as any;
     `.trim();
 
-const afterDedupeImport = `
-import {MuiIcons, test2, test4, test5, test6, test7} from "@sui/deps-material";
-import {test1} from "@nemui/material";
-import {test3, test8} from "antd";
-import {test9} from "@mui/material-fake";
-import {test10} from "antd/lib";
-import {Container} from "@sui/deps-ioc";
-import test11, {test12} from "colors";
-import * as colors from "colors";
-import "odometer/themes/odometer-theme-default.css";
-import {DataKey, dataKeysToDataTree} from "@sui/util-chore";
-import {Test} from "@sui/ui-old-react";
-
-const a = <MuiIcons.Menu attr="1">2</MuiIcons.Menu>;
-const b = <MuiIcons.Menu attr="1"/>;
-
-const c = <MuiIcons.DeleteForever attr="1">2</MuiIcons.DeleteForever>;
-const d = <MuiIcons.DeleteForever attr="1"/>;
-
-const f = <DrawerVisibleButton
-            edge="start"
-            disableRotate={isMobile}
-            icon={isMobile && <MuiIcons.Menu/>}
-          />;
-const g = MuiIcons.Menu as any;
-    `.trim();
-
 describe("sui-migrator generator", () => {
-  it("remapImports", () => {
-    expect(remapImports("deps-antd", source).trim()).toBe(afterRemapImports);
-  });
-
-  it("remapIcons", () => {
-    expect(remapIcons(afterRemapImports).trim()).toBe(afterRemapIcons);
-  });
-
   it("remapOldPackages", () => {
     expect(remapOldPackages(afterRemapIcons).trim()).toBe(afterRemapOldPackages);
-  });
-
-  it("dedupeImport", () => {
-    expect(dedupeImport(afterRemapOldPackages).trim()).toBe(afterDedupeImport);
   });
 });
