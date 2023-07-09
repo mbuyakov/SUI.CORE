332eab1b0afd581a864d7385cfb4f071b7c2a3b1
el1191 <el1191@woyd.ru>
2023-07-09 05:21:02 +0300

    test(nx-plugin): separate test for dedupeImport

---
+++ js/packages/nx-plugin/src/generators/sui-migrator/modules/dedupeImport.spec.ts
@@ -0,0 +1,70 @@
+import {dedupeImport} from "./dedupeImport";
+
+describe("dedupe import", () => {
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should leave as is", () => {
+    const consoleLog = jest.spyOn(console, "log");
+    const source = `
+import {a} from "a";
+import {b as b} from "b";
+import c from "c";
+import * as d from "d";
+import "a";
+    `.trim();
+    expect(dedupeImport(source)).toBe(source);
+    expect(consoleLog).not.toBeCalled();
+  });
+
+  it("should leave as in named import & namespace", () => {
+    const consoleLog = jest.spyOn(console, "log");
+    expect(dedupeImport(`
+import {a} from "a";
+import {b as b} from "b";
+import c from "c";
+import * as d from "a";
+import "a";
+    `.trim())).toMatchSnapshot();
+    expect(consoleLog).toBeCalledTimes(0);
+  });
+
+  it("should merge multiple named import", () => {
+    const consoleLog = jest.spyOn(console, "log");
+    expect(dedupeImport(`
+import {a} from "a";
+import {b as b} from "a";
+import c from "c";
+import * as d from "d";
+import "a";
+    `.trim())).toMatchSnapshot();
+    expect(consoleLog).toBeCalledTimes(1);
+  });
+
+  it("should merge named import & default", () => {
+    const consoleLog = jest.spyOn(console, "log");
+    expect(dedupeImport(`
+import {a} from "a";
+import {b as b} from "b";
+import c from "a";
+import * as d from "d";
+import "a";
+    `.trim())).toMatchSnapshot();
+    expect(consoleLog).toBeCalledTimes(1);
+  });
+
+  it("should merge named import & default + namespace on next line", () => {
+    const consoleLog = jest.spyOn(console, "log");
+    expect(dedupeImport(`
+import {a} from "a";
+import {b as b} from "a";
+import {f} from "b";
+import c from "a";
+import * as d from "a";
+import "a";
+    `.trim())).toMatchSnapshot();
+    expect(consoleLog).toBeCalledTimes(1);
+  });
+});
