b4727abb228bc71652c27d3f63c268bbba75cfb4
el1191 <el1191@woyd.ru>
2023-08-05 20:27:34 +0300

    chore: fix code coverage

--- js/jest.preset.js
+++ js/jest.preset.js
@@ -12,5 +12,5 @@ module.exports = {
    * More info: https://jestjs.io/docs/upgrading-to-jest29#snapshot-format
    */
   snapshotFormat: { escapeString: true, printBasicPrototype: true },
-  coverageReporters: ["html", "lcov"],
+  coverageReporters: ["lcov"],
 };
