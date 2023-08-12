f91777de7d858b9543a42854fb86188fe96f0de5
el1191 <el1191@woyd.ru>
2023-08-13 00:04:47 +0300

    feat(deps-antd): move antdMissedExport

--- js/packages/deps-antd/src/additionalExports.ts
+++ js/packages/deps-antd/src/additionalExports.ts
@@ -1,4 +1,16 @@
 export {ThemeConfig} from "antd5/es/config-provider/context";
 export {ArgsProps} from "antd5/es/notification/interface";
 export {default as ruRU} from "antd5/locale/ru_RU";
+export {CustomIconComponentProps} from "@ant-design/icons/lib/components/Icon";
 export type CardType = "inner";
+export type {ButtonType, ButtonGroupProps} from "antd5/lib/button";
+export type {SelectValue} from "antd5/lib/select";
+export type {CheckboxChangeEvent} from "antd5/lib/checkbox";
+export type {SizeType} from "antd5/lib/config-provider/SizeContext";
+export type {TextAreaProps} from "antd5/lib/input/TextArea";
+export type {FormListProps} from "antd5/lib/form/FormList";
+export type {NamePath} from "antd5/lib/form/interface";
+export type {ButtonHTMLType} from "antd5/lib/button/buttonHelpers";
+export type {ColumnType} from "antd5/lib/table";
+export type {RcFile} from "antd5/lib/upload/interface";
+export type {PresetColorType} from "antd5/lib/_util/colors";
