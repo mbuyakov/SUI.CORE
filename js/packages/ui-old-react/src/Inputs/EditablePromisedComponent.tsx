b9a6e17d7e353057414735779150132dfc0b8ba8
a.galimov <ites777@gmail.com>
2023-08-25 13:29:46 +0300

    cherrypick(ui-old-react): onClick to EditablePromisedComponent.tsx

--- js/packages/ui-old-react/src/Inputs/EditablePromisedComponent.tsx
+++ js/packages/ui-old-react/src/Inputs/EditablePromisedComponent.tsx
@@ -15,8 +15,8 @@ export interface IEditablePromisedComponentProps<T> {
   children: React.ReactElement<IPromisedBaseProps<T>>;
   disableEdit?: boolean;
   editRoles?: string[];
-
   nonEditRender?(value: T): React.ReactNode;
+  onClick?: () => unknown;
 }
 
 export class EditablePromisedComponent<T>
@@ -62,7 +62,10 @@ export class EditablePromisedComponent<T>
                             {editMode ? React.cloneElement(this.props.children, {promise: this.getPromise}) : (this.props.nonEditRender || this.DEFAULT_RENDERER).apply(null, [this.props.children.props.defaultValue])}
                             {(editMode || editAllowed) && (
                               <IconButton
-                                onClick={this.switchEdit}
+                                onClick={(): void => {
+                                  this.props.onClick && this.props.onClick()
+                                  this.switchEdit()
+                                }}
                                 style={{marginLeft: 6, padding: 0}}
                                 size="small"
                               >
