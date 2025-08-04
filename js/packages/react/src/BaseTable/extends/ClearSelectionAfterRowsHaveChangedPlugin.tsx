154fbb71211a3d8829762a38044c59182b2535ea
Nikita Kostin <n.kostin@cloudcom.ru>
2025-08-04 13:41:51 +0300

    Create ClearSelectionAfterRowsHaveChangedPlugin

---
+++ js/packages/react/src/BaseTable/extends/ClearSelectionAfterRowsHaveChangedPlugin.tsx
@@ -0,0 +1,41 @@
+/* tslint:disable:no-magic-numbers */
+import {ComputedFn, Getter, Plugin} from '@devexpress/dx-react-core';
+import autobind from "autobind-decorator";
+import React from "react";
+
+export interface IClearSelectionAfterRowsHaveChangedPluginProps {
+  selection?: any[];
+
+  clearSelection(): void;
+}
+
+export class ClearSelectionAfterRowsHaveChangedPlugin extends React.Component<IClearSelectionAfterRowsHaveChangedPluginProps> {
+
+  private lastSavedIds: string[];
+
+  public render(): JSX.Element {
+    return (
+      <Plugin name="ClearSelectionAfterRowsHaveChanged">
+        <Getter name="rows" computed={this.computedForWatcher("rows")}/>
+      </Plugin>
+    );
+  }
+
+  @autobind
+  private computedForWatcher(property: string): ComputedFn {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (getters): any => {
+      const ids = getters.rows.map(it => it?.id).filter(Boolean);
+
+      if (!this.lastSavedIds?.length) {
+        this.lastSavedIds = ids;
+      } else if (this.props.selection?.length && (this.lastSavedIds?.length != ids?.length || !this.lastSavedIds.every(id => ids.includes(id)))) {
+        this.props.clearSelection();
+        this.lastSavedIds = ids;
+      }
+
+      return getters[property];
+    }
+  }
+
+}
